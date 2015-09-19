666246d242154b15ea497009474c3fa8e9fe40f5
Rob Story <rob@simple.com>
2015-09-19 14:50:40 -0700

    Start histogram methods and tests
    Update to dataframe
    
    Changes to get hist working
    
    Some refactoring to get tests passing
    
    Pull in config on trigger
    
    Whitespace
    
    TST: Add one more test for passing types directly
    
    STY

--- altair/tests/test_api.py
+++ altair/tests/test_api.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+import numpy as np
+import pandas as pd
+
+from altair import api
 
 from .. import api, spec
 
@@ -54,13 +58,12 @@ def test_markers():
     for marktype in VALID_MARKTYPES:
         spec.mark(marktype)
         assert spec.marktype == marktype
-        
+
     # call, e.g. spec.point()
     for marktype in VALID_MARKTYPES:
         method = marktype
         getattr(spec, method)()
         assert spec.marktype == marktype
-
 
 def test_encode():
     data = dict(col1=[1.0, 2.0, 3.0],
@@ -119,7 +122,6 @@ def test_encode_types():
         assert getattr(spec.encoding, key).name == name
         assert getattr(spec.encoding, key).type == typ
 
-
 def test_infer_types():
     data = dict(col1=[1.0, 2.0, 3.0],
                 col2=[0.1, 0.2, 0.3],
@@ -139,5 +141,53 @@ def test_infer_types():
         name, typ = val
         assert getattr(spec.encoding, key).name == name
         assert getattr(spec.encoding, key).type == typ
-    
-    
+
+def test_hist():
+    data = dict(x=[1, 2, 3],
+                y=[4, 5, 6])
+
+    viz1 = api.Viz(data).hist(x='foo')
+    assert viz1.encoding.x.name == "foo"
+    assert viz1.encoding.x.bin.maxbins == 0
+    assert viz1.encoding.y.name == "*"
+    assert viz1.encoding.y.type == "Q"
+    assert viz1.encoding.y.aggregate == "count"
+
+    viz2 = api.Viz(data).hist(x="foo", bins=30)
+    assert viz2.encoding.x.bin.maxbins == 30
+    expected = {'data': {'formatType': 'json',
+                'values': [{'x': 1, 'y': 4}, {'x': 2, 'y': 5},
+                           {'x': 3, 'y': 6}]},
+                'encoding': {'x': {'bin': {'maxbins': 30}, 'name': 'foo'},
+                'y': {'aggregate': 'count',
+                      'bin': False,
+                      'name': '*',
+                      'type': 'Q'}},
+                'marktype': 'bar'}
+
+    viz3 = api.Viz(data).hist(x="foo:O",
+        color=api.Color(shorthand="bar", type="N")
+    )
+    assert viz3.encoding.x.name == "foo"
+    assert viz3.encoding.x.type == "O"
+
+    expected = {'data': {'formatType': 'json',
+                'values': [{'x': 1, 'y': 4}, {'x': 2, 'y': 5},
+                           {'x': 3, 'y': 6}]},
+                'encoding': {'x': {'bin': {}, 'name': 'foo', 'type': 'O'},
+                             'y': {'aggregate': 'count',
+                                   'bin': False,
+                                   'name': '*',
+                                   'type': 'Q'},
+                             'color': {'bin': False,
+                                       'name': 'bar',
+                                       'opacity': 1.0,
+                                       'type': 'N',
+                                       'value': '#4682b4'}},
+                'marktype': 'bar'}
+
+    assert viz3.to_dict() == expected
+
+    viz4 = api.Viz(data).hist(x=api.X(shorthand="foo", bin=api.Bin(maxbins=40)))
+    assert viz4.encoding.x.name == "foo"
+    assert viz4.encoding.x.bin.maxbins == 40
