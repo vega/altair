f6b7e341e4d86f8de9bc82bd1f2b47e8d8038f79
Jake VanderPlas <jakevdp@gmail.com>
2015-09-19 16:20:44 -0700

    TST: add initial API tests

--- altair/tests/test_api.py
+++ altair/tests/test_api.py
@@ -1,10 +1,48 @@
-from altair import api
+import pandas as pd
 
-def test_contains():
+from .. import api, spec
+
+
+VALID_MARKTYPES = spec.SPEC['properties']['marktype']['enum']
+
+
+def test_empty_data():
     d = api.Data()
     assert d.formatType=='json'
     assert 'formatType' in d
-    assert d.url==''
     assert 'url' not in d
-    assert d.data==[]
     assert 'data' not in d
+
+
+def test_dict_data():
+    data = dict(x=[1, 2, 3],
+                y=[4, 5, 6])
+    spec = api.Viz(data)
+    assert spec.data == data
+
+
+#def test_dataframe_data():
+#    datadict = dict(x=[1, 2, 3],
+#                    y=[4, 5, 6])
+#    data = pd.DataFrame(datadict)
+#    spec = api.Viz(data)
+#    assert spec.data == datadict
+
+
+def test_markers():
+    data = dict(x=[1, 2, 3],
+                y=[4, 5, 6])
+
+    spec = api.Viz(data)
+
+    # call, e.g. spec.mark('point')
+    for marktype in VALID_MARKTYPES:
+        spec.mark(marktype)
+        assert spec.marktype == marktype
+        
+    # call, e.g. spec.mark_point()
+    for marktype in VALID_MARKTYPES:
+        method = 'mark_' + marktype
+        getattr(spec, method)()
+        assert spec.marktype == marktype
+    
