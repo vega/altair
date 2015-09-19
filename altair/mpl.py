1e98b394a7e06bc255c2ab0020bcf74e9e2043c3
Thomas A Caswell <tcaswell@bnl.gov>
2015-09-19 02:11:23 -0400

    ENH: first pass at plotting

--- altair/mpl.py
+++ altair/mpl.py
@@ -1,5 +1,61 @@
-def render(vega_lite_spec, data=None):
+from matplotlib import pyplot as plt
+import pandas as pd
+from .spec import SPEC
+
+
+def _vl_line(ax, encoding, data):
+    return ax.plot(encoding.x.name, encoding.y.name, data=data)
+
+
+def _vl_area(ax, encoding, data):
+    ln, = ax.plot(encoding.x.name, encoding.y.name, data=data)
+    area = ax.fill_between(encoding.x.name, encoding.y.name, data=data)
+    return ln, area
+
+
+def _vl_point(ax, encoding, data):
+    ln, = ax.plot(encoding.x.name, encoding.y.name, data=data,
+                  linestyle='none', marker='o')
+    return ln,
+
+
+def _vl_bar(ax, encoding, data):
+    return ax.bar(encoding.x.name, encoding.y.name, data=data)
+
+
+def render(vls):
     """Render a vega-lite spec using matplotlib
 
+    Parameters
+    ----------
+    vls : dict
+        A dictionary complying with the vega-lite spec
+
+    Returns
+    -------
+    fig : Figure
+        The figure created
+
+    ax_list : list
+        The list of axes created
+
+    arts : dict
+        Dictionary keyed on Axes of the artists created
     """
-    pass
+
+    encoding = vls.encoding
+    data = vls.data
+    plot_func = _MARK_DISPATCHER[vls.marktype]
+    fig, ax = plt.subplots()
+
+    return plot_func(ax, encoding, data)
+
+
+_MARK_DISPATCHER = {'area': _vl_area,      # fill below line
+                    'bar': _vl_bar,        # bar
+                    'circle': None,        # ??
+                    'line': _vl_line,      # line
+                    'point': _vl_point,    # scatter
+                    'square': None,        # ??
+                    'text': None,          # ??
+                    'tick': None}          # ??
