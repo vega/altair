fd1bafa860b919311796f0792ebf7f0a722f408b
freeman <the.freeman.lab@gmail.com>
2015-09-19 21:17:43 -0700

    Handle scalar conversions

--- altair/html.py
+++ altair/html.py
@@ -1,6 +1,7 @@
 import os
 import jinja2
 import json
+import numpy
 
 def render(spec, width=None, height=None):
 	
@@ -15,7 +16,14 @@ def render(spec, width=None, height=None):
 	base = open(location).read()
 	d = spec.to_dict()
 	d['config'] = {'width': width, 'height': height}
-	spec = escape(json.dumps(d))
+
+	class NumpyConvert(json.JSONEncoder):
+		def default(self, obj):
+			if isinstance(obj, numpy.generic):
+				return numpy.asscalar(obj)
+			return json.JSONEncoder.default(self, obj)
+
+	spec = escape(json.dumps(d, cls=NumpyConvert))
 	fields = {'spec': spec, 'width': width, 'height': height}
 	t = Template(base)
 	html = t.render(**fields)
