1a821eab01d8dc67768214a80cf97c39ef022aab
Brian E. Granger <ellisonbg@gmail.com>
2015-09-19 22:51:36 -0700

    Adding config section.

--- altair/html.py
+++ altair/html.py
@@ -4,27 +4,30 @@ import json
 import numpy
 
 def render(spec, width=None, height=None):
-	
-	from jinja2 import Template, escape
 
-	if width is None:
-		width = 500;
-	if height is None:
-		height = 300
+    from jinja2 import Template, escape
 
-	location = os.path.join(os.path.dirname(__file__), 'templates/template.html')
-	base = open(location).read()
-	d = spec.to_dict()
-	d['config'] = {'width': width, 'height': height}
+    if width is not None:
+        spec.config.width = width
+    else:
+        width = spec.config.width
+    if height is not None:
+        spec.config.height = height
+    else:
+        height = spec.config.height
 
-	class NumpyConvert(json.JSONEncoder):
-		def default(self, obj):
-			if isinstance(obj, numpy.generic):
-				return numpy.asscalar(obj)
-			return json.JSONEncoder.default(self, obj)
+    location = os.path.join(os.path.dirname(__file__), 'templates/template.html')
+    base = open(location).read()
+    d = spec.to_dict()
 
-	spec = escape(json.dumps(d, cls=NumpyConvert))
-	fields = {'spec': spec, 'width': width, 'height': height}
-	t = Template(base)
-	html = t.render(**fields)
-	return html
+    class NumpyConvert(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, numpy.generic):
+                return numpy.asscalar(obj)
+            return json.JSONEncoder.default(self, obj)
+
+    spec = escape(json.dumps(d, cls=NumpyConvert))
+    fields = {'spec': spec, 'width': width, 'height': height}
+    t = Template(base)
+    html = t.render(**fields)
+    return html
