c24841b3ac1c2cd8ec40aee1141444be3f2f451e
Brian E. Granger <ellisonbg@gmail.com>
2015-09-19 21:53:18 -0700

    Fixing naming of mark_

--- altair/tests/test_html.py
+++ altair/tests/test_html.py
@@ -5,6 +5,6 @@ def test_render():
     data = dict(x=[1, 2, 3],
                 y=[4, 5, 6])
     spec = api.Viz(data)
-    s = spec.encode(x='x:Q', y='y:Q').mark_line()
+    s = spec.encode(x='x:Q', y='y:Q').line()
     r = html.render(s)
     assert r is not None
