ce991cf9b8526ee52431834e455ecdfc6cd58052
Alejandro Guerrero Gonzalez <alexgg@Alejandros-MacBook-Pro.local>
2015-11-02 18:02:16 -0800

    Does not refresh. Confused as to what on_render should do

--- altair/view.py
+++ altair/view.py
@@ -1,214 +1,218 @@
 import traitlets as T
 import ipywidgets as w
+import qgrid
+import json
+from qgrid.grid import QGridWidget, defaults
+from IPython.display import display
+import pandas as pd
 
 from .api import Encoding, X, Y, Row, Col, Shape, Size, Color, Detail, Text
 from .utils import infer_vegalite_type
 
 
-import qgrid
-qgrid.nbinstall(overwrite=True)
-qgrid.set_defaults(remote_js=True, precision=4)
-import json
-from qgrid.grid import QGridWidget, defaults
-from IPython.display import display, HTML
-import pandas as pd
-
-
-# ip = get_ipython()
-# ip.display_formatter.ipython_display_formatter.for_type_by_name('pandas.core.frame',
-#                                                                 'DataFrame',
-#                                                                 display_dataframe)
-# ip.display_formatter.ipython_display_formatter.for_type_by_name('pandas.core.series',
-#                                                                 'Series',
-#                                                                 display_dataframe)
-
 class AltairWidget(object):
+    _shelves = ('x', 'y', 'row', 'col', 'shape', 'color', 'size', 'detail', 'text')
 
-    def __init__(self, **kwargs):
-        pass
+    _shelf_types = {
+        'x': X,
+        'y': Y,
+        'row': Row,
+        'col': Col,
+        'shape': Shape,
+        'color': Color,
+        'detail': Detail,
+        'text': Text,
+        'size': Size
+    }
 
+    def __init__(self, viz, shelves=_shelves):
+        self.viz = viz
+        self.shelves = shelves
 
-def get_qgrid_widget(df):
-    # remote_js = defaults.remote_js
-    precision = defaults.precision
-    grid_options = {'forceFitColumns': False, 'defaultColumnWidth': 100}
-    return QGridWidget(df=df,
-                       precision=precision,
-                       grid_options=json.dumps(grid_options),
-                       remote_js=False)
+        qgrid.nbinstall(overwrite=True)
+        qgrid.set_defaults(remote_js=True, precision=4)
 
+        self._shelf_view_factories = {
+            'x': self._create_shelf_view,
+            'y': self._create_shelf_view,
+            'row': self._create_shelf_view,
+            'col': self._create_shelf_view,
+            'shape': self._create_shelf_view,
+            'color': self._create_shelf_view,
+            'detail': self._create_shelf_view,
+            'text': self._create_shelf_view,
+            'size': self._create_shelf_view
+        }
 
+        # Create output area
+        self.output = w.Output()
+        self.output.children = []
 
+        # Create widget
+        self.widget = self._create_render_view()
 
-_shelves = ('x', 'y', 'row', 'col', 'shape', 'color', 'size', 'detail', 'text')
+    def render(self):
+        return self.widget
 
-_shelf_types = {
-    'x': X,
-    'y': Y,
-    'row': Row,
-    'col': Col,
-    'shape': Shape,
-    'color': Color,
-    'detail': Detail,
-    'text': Text,
-    'size': Size
-}
-
-def create_dropdown_view(parent, attr):
-    options = parent.traits()[attr].values
-    value = getattr(parent, attr)
-    dd = w.Dropdown(options=options, value=value)
-    T.link((parent, attr), (dd, 'value'))
-    return dd
-
-
-def find_columns_and_types(df):
-    columns = []
-    types = []
-    for c in df.columns:
-        t = infer_vegalite_type(df[c])
-        columns.append(c)
-        types.append(t)
-    return columns, types
-
-
-def create_shelf_view(model, columns, types):
-    opts = list(zip(['-']+columns, ['']+columns))
-    column_view = w.Dropdown(options=opts, description=model.shelf_name, value='')
-    type_view = w.Dropdown(options={'Q':'Q','T':'T','O':'O','N':'N'}, value='Q')
-    shelf_view = w.HBox([column_view, type_view])
-    def on_column_changed(name, value):
-        if value == '':
-            type_view.value = 'Q'
-        else:
-            index = columns.index(value)
-            type_view.value = types[index]
-    column_view.on_trait_change(on_column_changed, 'value')
-    T.link((model, 'name'),(column_view, 'value'))
-    T.link((type_view, 'value'), (model, 'type'))
-    return shelf_view
-
-
-_shelf_view_factories = {
-    'x': create_shelf_view,
-    'y': create_shelf_view,
-    'row': create_shelf_view,
-    'col': create_shelf_view,
-    'shape': create_shelf_view,
-    'color': create_shelf_view,
-    'detail': create_shelf_view,
-    'text': create_shelf_view,
-    'size': create_shelf_view
-}
-
-
-def create_encoding_view(enc, columns, types, shelves=_shelves):
-    vbox = w.VBox()
-    children = []
-    children.append(w.HTML('Encoding:', width='148px', height='32px'))
-
-    for shelf in shelves:
-        if getattr(enc, shelf, None) is None:
-            setattr(enc, shelf, _shelf_types[shelf](''))
-        factory = _shelf_view_factories[shelf]
-        view = factory(getattr(enc, shelf), columns, types)
-        children.append(view)
-    
-    vbox.children = children
-    return vbox
-
-
-def create_viz_types_buttons(viz, output):
-    hbox = w.HBox()
-
-    heading = w.HTML('Mark:', width='50px', height='32px')
-
-    def on_render_table(*args):
-        with output:
-            output.clear_output()
-
-            data = viz.data
-            if isinstance(data, pd.Series):
-                df = pd.DataFrame(data)
-            else:
-                df = data
-            display(get_qgrid_widget(df))
-
-            # display(data)
-
-    def on_render_area(*args):
-        with output:
-            viz.area()
-            output.clear_output()
-            print(viz)
-
-    def on_render_line(*args):
-        with output:
-            viz.line()
-            output.clear_output()
-            print(viz)
-
-    def on_render_bar(*args):
-        with output:
-            viz.bar()
-            output.clear_output()
-            print(viz)
-
-    table_button = w.Button(description='Table')
-    table_button.on_click(on_render_table)
-
-    area_button = w.Button(description='Area')
-    area_button.on_click(on_render_area)
-
-    line_button = w.Button(description='Line')
-    line_button.on_click(on_render_line)
-
-    bar_button = w.Button(description='Bar')
-    bar_button.on_click(on_render_bar)
-
-    hbox.children = [heading, table_button, area_button, line_button, bar_button]
-    return hbox
-
-
-def create_viz_view(viz, shelves=_shelves):
-    columns, types = find_columns_and_types(viz.data)
-    vbox = w.VBox()
-    if not hasattr(viz, 'encoding'):
-        setattr(viz, 'encoding', Encoding())
-    encoding = create_encoding_view(viz.encoding, columns, types, shelves=shelves)
-    vbox.children = [encoding]
-    return vbox
-
-
-def create_render_view(v, shelves=_shelves):
-    # Create output area
-    output = w.Output()
-
-    # Create render button that currently prints v
-    render_button = w.Button(description='Render', width=20)
-    def on_render(*args):
-        with output:
+    def on_render(self, *args):
+        with self.output:
             # Not exposing this yet because of bugs in the renderers that break this.
             # Printing instead.
             # v.render()
-            output.clear_output()
-            print(v)
-    render_button.on_click(on_render)
+            # print(self.viz)
+            pass
+        self._clear_output()
+        to_render = w.Textarea(value=str(self.viz))
+        self.output.children = [to_render]
 
-    # Create types of viz hbox
-    viz_types_hbox = create_viz_types_buttons(v, output)
+    def _clear_output(self):
+        for c in self.output.children:
+            print(c)
+            c.visible = False
+        self.output.clear_output()
 
-    # Append render to types. This should be automatic afterwards.
-    # viz_types_hbox.children.append(render_button)
+    def _create_render_view(self):
+        # Create render button
+        render_button = w.Button(description='Render', width=20)
+        render_button.on_click(self.on_render)
 
-    # Create vbox for shelves
-    shelves_view = create_viz_view(v, shelves)
+        # Create types of viz hbox
+        viz_types_hbox = self._create_viz_types_buttons()
 
-    # Create left pane with all controls
-    left_pane = w.VBox()
-    left_pane.children = [render_button, viz_types_hbox, shelves_view]
+        # TODO: Append render to types. This should be automatic afterwards.
 
-    final_pane = w.HBox()
-    final_pane.children = [left_pane, output]
+        # Create vbox for shelves
+        shelves_view = self._create_viz_view()
 
-    return final_pane
+        # Create left pane with all controls
+        left_pane = w.VBox()
+        left_pane.children = [render_button, viz_types_hbox, shelves_view]
+
+        final_pane = w.HBox()
+        final_pane.children = [left_pane, self.output]
+
+        return final_pane
+
+    def _create_viz_view(self):
+        columns, types = self._find_columns_and_types(self.viz.data)
+        vbox = w.VBox()
+        if not hasattr(self.viz, 'encoding'):
+            setattr(self.viz, 'encoding', Encoding())
+        encoding = self._create_encoding_view(columns, types)
+        vbox.children = [encoding]
+        return vbox
+
+    def _create_viz_types_buttons(self):
+        hbox = w.HBox()
+
+        heading = w.HTML('Mark:', width='50px', height='32px')
+
+        def on_render_table(*args):
+            with self.output:
+                self.output.clear_output()
+
+                data = self.viz.data
+                if isinstance(data, pd.Series):
+                    df = pd.DataFrame(data)
+                else:
+                    df = data
+                display(self._get_qgrid_widget(df))
+
+                # display(data)
+
+        def on_render_area(*args):
+            with self.output:
+                self.viz.area()
+                self.output.clear_output()
+                print(self.viz)
+
+        def on_render_line(*args):
+            with self.output:
+                self.viz.line()
+                self.output.clear_output()
+                print(self.viz)
+
+        def on_render_bar(*args):
+            with self.output:
+                self.viz.bar()
+                self.output.clear_output()
+                print(self.viz)
+
+        table_button = w.Button(description='Table')
+        table_button.on_click(on_render_table)
+
+        area_button = w.Button(description='Area')
+        area_button.on_click(on_render_area)
+
+        line_button = w.Button(description='Line')
+        line_button.on_click(on_render_line)
+
+        bar_button = w.Button(description='Bar')
+        bar_button.on_click(on_render_bar)
+
+        hbox.children = [heading, table_button, area_button, line_button, bar_button]
+        return hbox
+
+    @staticmethod
+    def _create_shelf_view(model, columns, types):
+        opts = list(zip(['-']+columns, ['']+columns))
+        column_view = w.Dropdown(options=opts, description=model.shelf_name, value='')
+        type_view = w.Dropdown(options={'Q': 'Q', 'T': 'T', 'O': 'O', 'N': 'N'}, value='Q')
+        shelf_view = w.HBox([column_view, type_view])
+
+        def on_column_changed(name, value):
+            if value == '':
+                type_view.value = 'Q'
+            else:
+                index = columns.index(value)
+                type_view.value = types[index]
+
+        column_view.on_trait_change(on_column_changed, 'value')
+        T.link((model, 'name'), (column_view, 'value'))
+        T.link((type_view, 'value'), (model, 'type'))
+        return shelf_view
+
+    @staticmethod
+    def _find_columns_and_types(df):
+        columns = []
+        types = []
+        for c in df.columns:
+            t = infer_vegalite_type(df[c])
+            columns.append(c)
+            types.append(t)
+        return columns, types
+
+    @staticmethod
+    def _create_dropdown_view(parent, attr):
+        options = parent.traits()[attr].values
+        value = getattr(parent, attr)
+        dd = w.Dropdown(options=options, value=value)
+        T.link((parent, attr), (dd, 'value'))
+        return dd
+
+    def _create_encoding_view(self, columns, types):
+        enc = self.viz.encoding
+
+        vbox = w.VBox()
+        children = list()
+        children.append(w.HTML('Encoding:', width='148px', height='32px'))
+
+        for shelf in self.shelves:
+            if getattr(enc, shelf, None) is None:
+                setattr(enc, shelf, self._shelf_types[shelf](''))
+            factory = self._shelf_view_factories[shelf]
+            view = factory(getattr(enc, shelf), columns, types)
+            children.append(view)
+
+        vbox.children = children
+        return vbox
+
+    @staticmethod
+    def _get_qgrid_widget(df):
+        # remote_js = defaults.remote_js
+        precision = defaults.precision
+        grid_options = {'forceFitColumns': False, 'defaultColumnWidth': 100}
+        return QGridWidget(df=df,
+                           precision=precision,
+                           grid_options=json.dumps(grid_options),
+                           remote_js=False)
