95fbdef2f635b6de3df3a960176e6899e6b05b2f
Jake VanderPlas <jakevdp@gmail.com>
2016-05-06 11:49:02 -0700

    make SCHEMA_PATH absolute

--- altair/schema/_schema.py
+++ altair/schema/_schema.py
@@ -1,7 +1,8 @@
 import os
 import json
 
-SCHEMA_FILE = os.path.join(os.path.dirname(__file__), 'vega-lite-schema.json')
+SCHEMA_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                           'vega-lite-schema.json')
 
 with open(SCHEMA_FILE) as f:
     SCHEMA = json.load(f)
