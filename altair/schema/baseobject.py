0d6e69745a73c36e42a2630699a8f99177d80896
Jake VanderPlas <jakevdp@gmail.com>
2016-05-19 21:23:20 -0700

    BUG: fix baseobject __contains__ to not ignore numerical zeros

--- altair/schema/baseobject.py
+++ altair/schema/baseobject.py
@@ -11,11 +11,21 @@ class BaseObject(T.HasTraits):
     skip = []
 
     def __contains__(self, key):
-        value = getattr(self, key)
+        try:
+            value = getattr(self, key)
+        except AttributeError:
+            return False
+
+        # comparison to None will break, so check DataFrame specifically
         if isinstance(value, pd.DataFrame):
             return True
-        return ((value is not None)
-                and (not (not isinstance(value, bool) and not value)))
+        elif value is not None:
+            if isinstance(value, (int, float, bool)):
+                return True
+            else:
+                return bool(value)
+        else:
+            return False
 
     def to_dict(self):
         result = {}
