ecf691b77da1034841bde280a5f2fa231166876e
Jake VanderPlas <jakevdp@gmail.com>
2016-05-22 13:12:42 -0700

    TST: add auto-testing of all examples included in vega-lite

--- altair/examples/tests/test_examples.py
+++ altair/examples/tests/test_examples.py
@@ -1,5 +1,8 @@
-import pytest
+import os
 import pkgutil
+import json
+
+import pytest
 
 from ...datasets import connection_ok, URLError
 from ... import examples
@@ -14,6 +17,13 @@ def iter_submodules(package, include_packages=False):
         if not include_packages and ispkg:
             continue
         yield modname
+
+
+def iter_json_examples():
+    json_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                            '..', 'json'))
+    for json_file in os.listdir(json_dir):
+        yield os.path.join(json_dir, json_file)
 
 
 @pytest.mark.skipif(not connection_ok(), reason='No Internet Connection')
@@ -34,3 +44,20 @@ def test_examples_output(modname):
     # test to_altair methods
     v3 = eval(v.to_altair())
     assert v3.to_dict() == vdict
+
+
+@pytest.mark.skipif(not connection_ok(), reason='No Internet Connection')
+@pytest.mark.parametrize('json_path', iter_json_examples())
+def test_json_examples_round_trip(json_path):
+    """
+    Test that Altair correctly round-trips JSON with to_dict() and to_altair()
+    """
+    with open(json_path) as f:
+        json_dict = json.load(f)
+
+    v = Layer.from_dict(json_dict)
+    v_dict = v.to_dict()
+    assert v_dict == json_dict
+
+    v2 = eval(v.to_altair())
+    assert v_dict == v2.to_dict()
