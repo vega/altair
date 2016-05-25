231ed42e75cb1f8594aad21385d878faff610b99
Jake VanderPlas <jakevdp@gmail.com>
2016-05-25 15:30:36 -0700

    BUG: don't load schema on package import

--- altair/schema/__init__.py
+++ altair/schema/__init__.py
@@ -1,3 +1,3 @@
 from .baseobject import BaseObject
 from ._generated import *
-from ._schema import SCHEMA_FILE, SCHEMA
+from ._schema import SCHEMA_FILE, load_schema
--- altair/schema/_schema.py
+++ altair/schema/_schema.py
@@ -4,5 +4,7 @@ import json
 SCHEMA_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                            'vega-lite-schema.json')
 
-with open(SCHEMA_FILE) as f:
-    SCHEMA = json.load(f)
+def load_schema():
+    """Load the VegaLite Schema as a Python dictionary"""
+    with open(SCHEMA_FILE) as f:
+        return json.load(f)
