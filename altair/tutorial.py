7812a0e11d8c4094809d0b84d24b69ded1b439d2
Brian E. Granger <ellisonbg@gmail.com>
2016-07-10 23:18:57 -0500

    Updating tutorial function.

--- altair/tutorial.py
+++ altair/tutorial.py
@@ -2,40 +2,30 @@
 
 import os
 import shutil
+import glob
 from six.moves.urllib.request import urlretrieve
 
-TUTORIAL_DIR = 'AltairTutorial'
 
-BASE_URL = 'https://raw.githubusercontent.com/ellisonbg/altair/master/notebooks/'
+SRC_PATH = os.path.join(
+    os.path.split(os.path.abspath(__file__)
+)[0], 'notebooks')
 
-FILES = [
-    '01-Index.ipynb',
-    '02-Introduction.ipynb',
-    '03-ScatterCharts.ipynb',
-    '04-BarCharts.ipynb',
-    '05-LineCharts.ipynb',
-    '06-AreaCharts.ipynb',
-    '07-LayeredCharts.ipynb',
-    '08-GroupedRegressionCharts.ipynb',
-    '09-CarsDataset.ipynb',
-    '10-IrisPairgrid.ipynb'
-]
- 
- 
-def download_tutorial(overwrite=True):
-    """Download the Altair tutorial notebooks."""
-    if os.path.isdir(TUTORIAL_DIR) and overwrite:
-        print('Removing old tutorial directory: {}'.format(TUTORIAL_DIR))
-        shutil.rmtree(TUTORIAL_DIR, ignore_errors=True)
-    print('Downloading fresh tutorial directory: {}'.format(TUTORIAL_DIR))
-    os.mkdir(TUTORIAL_DIR)
-    for f in FILES:
-        urlretrieve(BASE_URL+f, os.path.join(TUTORIAL_DIR, f))
+DEST_PATH = './AltairTutorial'
+
+def copy_tutorial(overwrite=False):
+    """Copy the Altair tutorial notebooks into ./AltairTutorial."""
+    if os.path.isdir(DEST_PATH) and overwrite:
+        print('Removing old tutorial directory: {}'.format(DEST_PATH))
+        shutil.rmtree(DEST_PATH, ignore_errors=True)
+    if os.path.isdir(DEST_PATH):
+        raise RuntimeError('{} already exists, run with overwrite=True to discard existing files'.format(DEST_PATH))
+    print('Copying notebooks into fresh tutorial directory: {}'.format(DEST_PATH))
+    shutil.copytree(SRC_PATH, DEST_PATH)
 
 
-def tutorial(overwrite=True):
-    """Download the Altair tutorial notebooks and show a link in the notebook."""
-    download_tutorial(overwrite=overwrite)
+def tutorial(overwrite=False):
+    """Copy the Altair tutorial notebooks into ./AltairTutorial and show a link in the notebook."""
+    copy_tutorial(overwrite=overwrite)
     print('Click on the following notebooks to explore the tutorial:')
     from IPython.display import FileLinks, display
-    display(FileLinks(TUTORIAL_DIR))
+    display(FileLinks(DEST_PATH))
