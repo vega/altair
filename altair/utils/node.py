8e35d7b3f989fe34d63ace0f2c1082e933f096c1
Jake VanderPlas <jakevdp@gmail.com>
2016-09-13 08:51:09 -0700

    TST: add test of node/savechart functionality

--- altair/utils/node.py
+++ altair/utils/node.py
@@ -5,31 +5,30 @@ from __future__ import print_function
 
 import os
 import tempfile
-import random
 
 
-__all__ = ['savechart']
+__all__ = ['savechart', 'savechart_available']
 
-NODE_BIN_DIR = os.path.expanduser('~/node_modules/.bin')
 SUPPORTED_FILETYPES = ['png', 'svg']
 
 
-class NodeExecutor(object):
+class _NodeExecutor(object):
     """Class to execute vega/vega-lite conversion commands in nodejs
 
     Parameters
     ----------
     node_bin_dir : str
-        The directory containing binary executables installed by node
+        The directory containing binary executables installed by node.
+        If not specified, then ``npm root`` will be used to find it.
     verbose : bool (optional)
         If True (default) then print commands before executing them.
     """
-    def __init__(self, node_bin_dir=NODE_BIN_DIR, verbose=True):
+    def __init__(self, node_bin_dir=None, verbose=True):
         self.verbose = verbose
-        self.node_bin_dir = node_bin_dir
+        self.node_bin_dir = os.path.abspath(node_bin_dir or self.npm_root)
 
     def _exec(self, executable, inputfile, outputfile):
-        full_executable = os.path.join(NODE_BIN_DIR, executable)
+        full_executable = os.path.join(self.node_bin_dir, executable)
         if not os.path.exists(full_executable):
             raise ValueError('{0} not found'.format(full_executable))
         command = '{0} {1} > {2}'.format(full_executable,
@@ -59,9 +58,35 @@ class NodeExecutor(object):
     def vl2svg(self, inputfile, outputfile):
         self._chain('vl2vg', 'vg2svg', inputfile, outputfile)
 
+    @property
+    def npm_root(self):
+        rootdir = os.popen('npm root').read().strip()
+        if not os.path.exists(rootdir):
+            raise ValueError('npm root did not return a valid filename')
+        return os.path.join(rootdir, '.bin')
+
+    def savechart_available(self):
+        try:
+            root = self.npm_root
+        except:
+            return False
+        else:
+            root = root.strip()
+            if not os.path.exists(root):
+                return False
+            else:
+                L = os.listdir(root)
+                return ('vl2vg' in L and 'vg2png' in L and 'vg2svg' in L)
+
+
+def savechart_available(node_bin_dir=None):
+    node = _NodeExecutor(node_bin_dir=node_bin_dir)
+    return node.savechart_available()
+        
+
 
 def savechart(chart, filename, filetype=None,
-              node_bin_dir=NODE_BIN_DIR, verbose=True):
+              node_bin_dir=None, verbose=False):
     """EXPERIMENTAL means of saving a chart to png or svg
 
     Note that this requires several nodejs packages to be installed and
@@ -84,13 +109,11 @@ def savechart(chart, filename, filetype=None,
         The filetype to use (either 'svg' or 'png'). If not specified,
         it will be inferred from the filename.
     node_bin_dir : str
-        The directory containing binary executables installed by node
+        The directory containing binary executables installed by node.
+        If not specified, then ``npm root`` will be used to determine it.
     verbose : bool (optional)
         If True (default) then print commands before executing them.
     """
-    node = NodeExecutor(node_bin_dir=node_bin_dir,
-                        verbose=verbose)
-
     if filetype is None:
         base, ext = os.path.splitext(filename)
         filetype = ext[1:]
@@ -98,6 +121,12 @@ def savechart(chart, filename, filetype=None,
     if filetype not in SUPPORTED_FILETYPES:
         raise ValueError("Filetype {0} not valid: must be one of {1}"
                          "".format(filetype, SUPPORTED_FILETYPES))
+
+    node = _NodeExecutor(node_bin_dir=node_bin_dir,
+                         verbose=verbose)
+    if not node.savechart_available():
+        raise ValueError("Must install and configure nodejs tools "
+                         "to use savechart")
         
     with tempfile.NamedTemporaryFile(suffix='.json') as f:
         with open(f.name, 'w') as ff:
---
+++ altair/utils/tests/test_node.py
@@ -0,0 +1,42 @@
+import tempfile
+from xml.etree import ElementTree
+
+import pytest
+import pandas as pd
+
+from altair.utils.node import savechart, savechart_available
+from altair import Chart
+
+
+def consistent_with_png(filename):
+    """Check that the file has a header consistent with PNG"""
+    with open(filename, 'rb') as f:
+        arr = f.read()
+    return arr.startswith(b'\x89PNG\r\n')
+
+
+def consistent_with_svg(filename):
+    """Check that the file has a structure consistent with SVG"""
+    try:
+        e = ElementTree.parse(filename)
+    except ElementTree.ParseError:
+        return False
+    else:
+        keys = e.getroot().keys()
+        return set(keys) == {'width', 'version', 'class', 'height'}
+
+
+@pytest.mark.skipif(not savechart_available(),
+                    reason='node tools are not available')
+def test_savechart():
+    data = pd.DataFrame({'x': range(10),
+                         'y': range(10)})
+    chart = Chart(data).mark_point().encode(x='x', y='y')
+    
+    with tempfile.NamedTemporaryFile(suffix='.png') as f:
+        savechart(chart, f.name)
+        assert consistent_with_png(f.name)
+
+    with tempfile.NamedTemporaryFile(suffix='.svg') as f:
+        savechart(chart, f.name)
+        assert consistent_with_svg(f.name)
