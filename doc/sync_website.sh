ea8e61be18054d4525698121985ad2c4ed47113e
Jake VanderPlas <jakevdp@gmail.com>
2016-09-23 13:37:22 -0700

    DOC: add Make command to sync website

---
+++ doc/sync_website.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# get git hash for commit message
+GITHASH=$(git rev-parse HEAD)
+MSG="doc build for commit $GITHASH"
+cd _build
+
+# clone the repo if needed
+if test -d altair-viz.github.io; 
+then echo "using existing cloned astropy directory";
+else git clone git@github.com:altair-viz/altair-viz.github.io.git;
+fi
+
+# sync the website
+cd altair-viz.github.io
+git pull
+
+# remove all tracked files
+git ls-files -z | xargs -0 rm -f
+
+# sync files from html build
+rsync -r ../html/ ./
+
+# add commit, and push to github
+git add . --all
+git commit -m $MSG
+git push origin master
