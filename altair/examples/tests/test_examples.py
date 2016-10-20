cb442754b0919ea4a89cdff7bad7e01b283b628e
Jake VanderPlas <jakevdp@gmail.com>
2016-10-19 20:32:28 -0700

    MAINT: add-in missing examples

--- altair/examples/tests/test_examples.py
+++ altair/examples/tests/test_examples.py
@@ -6,6 +6,11 @@ from .. import iter_examples, load_example, iter_examples_with_metadata
 from ... import *
 
 
+def remove_empty_fields(spec):
+    """Remove empty entries from the spec"""
+    return {k:v for k, v in spec.items() if v}
+
+
 @pytest.mark.parametrize('example', iter_examples())
 def test_json_examples_round_trip(example):
     """
@@ -17,8 +22,10 @@ def test_json_examples_round_trip(example):
     v_dict = v.to_dict()
     assert v_dict == json_dict
 
+    # code generation discards empty function calls, and so we
+    # filter these out before comparison
     v2 = eval(v.to_altair())
-    assert v_dict == v2.to_dict()
+    assert v2.to_dict() == remove_empty_fields(json_dict)
 
 
 def test_load_example():
@@ -34,5 +41,3 @@ def test_metadata(D):
     expected_keys = {'spec', 'filename', 'category', 'name', 'title'}
     assert len(expected_keys - set(D.keys())) == 0
     assert 'data' in D['spec']
-    
-    
