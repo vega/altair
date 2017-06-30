fec706d7d3f4353dda6516e244880dda2616fcd1
Eoghan Ó Carragáin <eoghan.ocarragain@gmail.com>
2017-06-30 09:44:22 +0100

    correct default url for version 1 of vega-lite.js

--- altair/urls.py
+++ altair/urls.py
@@ -1,4 +1,4 @@
 D3_JS_URL = "https://d3js.org/d3.v3.min.js"
 VEGA_JS_URL = "https://vega.github.io/vega/releases/v2.6.5/vega.js"
-VEGALITE_JS_URL = "https://vega.github.io/vega-lite-1/vega-lite.js"
+VEGALITE_JS_URL = "https://vega.github.io/vega-lite-v1/vega-lite.js"
 VEGAEMBED_JS_URL = "https://vega.github.io/vega-editor/vendor/vega-embed.js"
