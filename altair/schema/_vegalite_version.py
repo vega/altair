91a3592d5667ccd04dc2f0963f680ccd44d1520e
Jake VanderPlas <jakevdp@gmail.com>
2017-08-09 10:24:52 -0700

    MAINT: download schema from new location

--- altair/schema/_vegalite_version.py
+++ altair/schema/_vegalite_version.py
@@ -1,4 +1,4 @@
-
 # Automatically written by tools/sync_vegalite.py
 # Do not modify this manually
-vegalite_version = '1.2.1'
+vegalite_version = 'v1.2.1'
+vegalite_schema_url = 'https://vega.github.io/schema/vega-lite/v1.2.1.json'
