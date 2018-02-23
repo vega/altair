7f9bb3c5933b718daf44a9b49ae946af98fef6c4
afonit <afonit@gmail.com>
2018-02-23 17:44:34 -0500

    adding basic scatter plot from vega-lite docs

---
+++ altair/vegalite/v2/examples/interactive_scatter_plot.py
@@ -0,0 +1,15 @@
+"""
+Interactive Colored Scatterplot
+-----------------
+This example shows how to make an interactive scatterplot.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.cars()
+
+chart = alt.Chart(source).mark_circle().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin').interactive()
