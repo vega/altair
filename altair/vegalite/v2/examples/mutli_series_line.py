634e7ceb94edd4bc296fd7ad8df1240b6817bb99
Eitan Lees <eitanlees@gmail.com>
2018-02-23 10:16:07 -0500

    Added a multi series line example

---
+++ altair/vegalite/v2/examples/mutli_series_line.py
@@ -0,0 +1,17 @@
+"""
+Multi Series Line Chart
+-----------------------
+
+This example shows how to make a multi series line chart of the daily closing stock prices for AAPL, AMZN, GOOG, IBM, and MSFT between 2000 and 2010.
+"""
+
+from altair import Chart
+from vega_datasets import data
+
+stocks = data.stocks()
+
+chart = Chart(stocks).mark_line().encode(
+    x = 'date', 
+    y = 'price', 
+    color = 'symbol'
+)
