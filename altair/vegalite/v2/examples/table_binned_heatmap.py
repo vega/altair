30ce4623ddf2cf44e4e81c0de0ccd422d99b9acb
afonit <afonit@gmail.com>
2018-02-24 16:08:40 -0500

    moved the encoding per Jake's instructions

--- altair/vegalite/v2/examples/table_binned_heatmap.py
+++ altair/vegalite/v2/examples/table_binned_heatmap.py
@@ -12,6 +12,6 @@ source = data.movies()
 chart = alt.Chart(source).mark_rect().encode(
     x = alt.X('IMDB_Rating', bin = alt.BinParams(maxbins=60)), 
     y = alt.Y('Rotten_Tomatoes_Rating', bin = alt.BinParams(maxbins=40)),
-    color = alt.Color('count(IMDB_Rating)'))
-    
-chart.config = {'range': {'heatmap': {'scheme': 'greenblue'}}}
+    color = alt.Color('count(IMDB_Rating)',
+                      scale=alt.Scale(scheme='greenblue'))
+)
