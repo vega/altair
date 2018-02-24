e6b7eedbd5897a75d3a3f37a63fe91ac3c10c061
afonit <afonit@gmail.com>
2018-02-24 13:39:58 -0500

    adding vega-lite example of table binned heatmap

---
+++ altair/vegalite/v2/examples/table_binned_heatmap.py
@@ -0,0 +1,17 @@
+"""
+Table Binned Heatmap
+-----------------------
+This example shows how to make a heatmap.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.movies()
+
+chart = alt.Chart(source).mark_rect().encode(
+    x = alt.X('IMDB_Rating', bin = alt.BinParams(maxbins=60)), 
+    y = alt.Y('Rotten_Tomatoes_Rating', bin = alt.BinParams(maxbins=40)),
+    color = alt.Color('count(IMDB_Rating)'))
+    
+chart.config = {'range': {'heatmap': {'scheme': 'greenblue'}}}
