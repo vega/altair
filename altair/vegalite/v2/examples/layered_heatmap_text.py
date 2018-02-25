87828cf2c1dc19cc610e3de710a802e256832a6a
Wasim Lorgat <mwlorgat@gmail.com>
2018-02-25 18:48:15 +0200

    Added layered heatmap + text example

---
+++ altair/vegalite/v2/examples/layered_heatmap_text.py
@@ -0,0 +1,28 @@
+"""
+Layering text over a heatmap
+----------------------------
+
+An example of a layered chart of text over a heatmap using the cars dataset.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+cars = data.cars.url
+
+heatmap = alt.Chart(cars).mark_rect().encode(
+    alt.X('Cylinders:O', scale=alt.Scale(paddingInner=0)),
+    alt.Y('Origin:O', scale=alt.Scale(paddingInner=0)),
+    alt.Color('count(*):Q')
+)
+
+text = alt.Chart(cars).mark_text(
+    color = 'white',
+    baseline = 'middle'
+).encode(
+    alt.X('Cylinders:O'),
+    alt.Y('Origin:O'),
+    alt.Text('count(*):Q')
+)
+
+chart = heatmap + text
