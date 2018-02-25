315fea4c553c62a4f75dcd8606f0797a55744657
afonit <afonit@gmail.com>
2018-02-25 08:57:54 -0500

    added bubble plot example from vega-lite

---
+++ altair/vegalite/v2/examples/bubble_plot.py
@@ -0,0 +1,16 @@
+"""
+Bubble Plot
+-----------------
+This example shows how to make a bubble plot.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.cars()
+
+chart = alt.Chart(source).mark_point().encode(
+    x = 'Horsepower',
+    y = 'Miles_per_Gallon',
+    size = 'Acceleration'
+)
