288441316db2bc4416a1364a1a8dcf39a0fa58e4
afonit <afonit@gmail.com>
2018-02-25 14:34:05 -0500

    added the steamgraph example from vega-lite

---
+++ altair/vegalite/v2/examples/steamgraph.py
@@ -0,0 +1,18 @@
+"""
+Streamgraph
+-----------------
+This example shows the steamgraph from vega-lite examples.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.unemployment_across_industries()
+
+
+chart = alt.Chart(source).mark_area().encode(
+    x = alt.X('date:T', timeUnit = 'yearmonth',
+              axis=alt.Axis(format='%Y', domain = False, tickSize = 0)),
+    y = alt.Y('sum(count)', stack = 'center'),
+    color = alt.Color('series', scale=alt.Scale(scheme='category20b'))
+    ).interactive()
