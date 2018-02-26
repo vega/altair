2c4eda4027d539e7892b0d6403b46312c75d7261
afonit <afonit@gmail.com>
2018-02-26 12:17:37 -0500

    added aggregate bar chart from vega-lite

---
+++ altair/vegalite/v2/examples/aggregate_bar_chart.py
@@ -0,0 +1,16 @@
+"""
+Aggregate Bar Chart
+-----------------
+This example is a bar chart showing the US population distribution of age groups in 2000..
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.population()
+
+chart = alt.Chart(source).mark_bar().encode(
+    x = alt.X('sum(people):Q', axis = alt.Axis(title = 'population')),
+    y = alt.Y('age:O', scale = alt.Scale(rangeStep = 17)))
+
+chart.transform = [{"filter": "datum.year == 2000"}]
