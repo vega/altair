88d418bc42d4e236ce0e34bfe7733c09b194511b
afonit <afonit@gmail.com>
2018-02-26 11:24:41 -0500

    added the vega-lite example of natural disasters

---
+++ altair/vegalite/v2/examples/natural_disasters.py
@@ -0,0 +1,20 @@
+"""
+Natural Disasters
+-----------------
+This example shows a visualization of global deaths from natural disasters.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.disasters()
+
+chart = alt.Chart(source).mark_circle(opacity = 0.8, stroke = 'black', strokeWidth = 1).encode(
+    x = alt.X('Year:O', axis = alt.Axis(labelAngle = 0)),
+    y = alt.X('Entity:N'),
+    size = alt.Size('Deaths:Q', scale = alt.Scale(range = [0, 5000]),
+                   legend = alt.Legend(title = 'Annual Global Deaths')),
+    color = alt.Color('Entity', legend = None)
+).properties(width=600, height=400)
+
+chart.transform = [{"filter": "datum.Entity !== 'All natural disasters'"}]
