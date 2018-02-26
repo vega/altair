bff1cd0739ddcb4ede704062f27fe63bfbf6a99b
afonit <afonit@gmail.com>
2018-02-26 13:01:46 -0500

    added vega-lite example: normalized stacked bar chart

---
+++ altair/vegalite/v2/examples/normalized_stacked_bar_chart.py
@@ -0,0 +1,20 @@
+"""
+Normalized Stacked Bar Chart
+-----------------------
+This example shows how to make a normalized stacked bar chart.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.population()
+
+chart = alt.Chart(source).mark_bar().encode(
+    x = alt.X('age:O', scale = alt.Scale(rangeStep = 17)), 
+    y = alt.Y('sum(people):Q', axis = alt.Axis(title = 'population'), stack = 'normalize'),
+    color = alt.Color('gender:N',
+                      scale=alt.Scale(
+            range = ["#EA98D2", "#659CCA"]))
+)
+chart.transform = [{"filter": "datum.year == 2000"},
+                  {"calculate": "datum.sex == 2 ? 'Female' : 'Male'", "as": "gender"}]
