4da5786a39df75848329fd32c91a9218849c24bb
afonit <afonit@gmail.com>
2018-02-26 17:14:07 -0500

    added example from vega-lite: one dot per zipcode

---
+++ altair/vegalite/v2/examples/one_dot_per_zipcode.py
@@ -0,0 +1,35 @@
+"""
+One Dot Per Zipcode
+-----------------------
+This example shows a geographical plot with one dot per zipcode.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+states = alt.UrlData(data.us_10m.url,
+                     format=alt.TopoDataFormat(type='topojson',
+                                               feature='states'))
+zipcodes = data.zipcodes()
+zipcodes = zipcodes[5000:10000]
+
+# US states background
+background = alt.Chart(states).mark_geoshape(
+    fill='lightgray',
+    stroke='white'
+).properties(
+    projection={'type': 'albersUsa'},
+    width=800,
+    height=500
+)
+
+# Zip Codes labeled on background
+points = alt.Chart(zipcodes).mark_circle(size = 1).encode(
+    #alt.Text('city', type='nominal'),
+    alt.X('longitude', type='longitude'),
+    alt.Y('latitude', type='latitude'),
+    color = 'digit:N'
+)
+
+points.transform = [{"calculate": "substring(datum.zip_code, 0, 1)", "as": "digit"}]
+chart = background + points
