b07749d36f4ed82e76f41cd4148c1f4e38e03075
afonit <afonit@gmail.com>
2018-02-27 09:30:47 -0500

    added vega-lite example: mean overlay..bar line layered chart

---
+++ altair/vegalite/v2/examples/mean_overlay_over_precipitiation_chart.py
@@ -0,0 +1,21 @@
+
+"""
+Layered Bar Chart with Line as Mean
+-----------------------
+This example shows mean overlay over precipitation chart.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.seattle_weather()
+
+bar = alt.Chart(source).mark_bar().encode(
+    x = alt.X('date:O', timeUnit = 'month'),
+    y = alt.Y('mean(precipitation):Q'))
+
+rule = alt.Chart(source).mark_rule(color = 'red',
+                                  size = 3).encode(
+    y = 'mean(precipitation)')
+    #size = alt.Size(value = 3))
+chart = bar + rule
