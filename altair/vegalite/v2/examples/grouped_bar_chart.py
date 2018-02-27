82552db7681066a8b669ead87f6671bf3d6ff407
afonit <afonit@gmail.com>
2018-02-27 12:26:52 -0500

    added vega-lite example: grouped bar chart

---
+++ altair/vegalite/v2/examples/grouped_bar_chart.py
@@ -0,0 +1,21 @@
+"""
+Grouped Bar Chart
+-----------------------
+This example shows a population broken out by gender and age for a specific year.
+"""
+
+import altair as alt
+from vega_datasets import data
+
+source = data.population()
+
+chart = alt.Chart(source).mark_bar(stroke = 'transparent').encode(
+    x = alt.X('gender:N', scale = alt.Scale(rangeStep = 12), axis = alt.Axis(title = '')),
+    y = alt.Y('sum(people):Q', axis = alt.Axis(title = 'population', grid = False)),
+    color = alt.Color('gender:N', scale = alt.Scale(range = ["#EA98D2", "#659CCA"])),
+    column = 'age:O')
+
+chart.transform = [{"filter": "datum.year == 2000"},
+    {"calculate": "datum.sex == 2 ? 'Female' : 'Male'", "as": "gender"}]
+
+chart.config = {"view": {"stroke": "transparent"}, "axis": {"domainWidth": 1}}
