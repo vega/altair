9caa08f5223fc8ec62fc2f9797170d70d58ccfca
Eitan Lees <eitanlees@gmail.com>
2018-02-28 14:55:55 -0500

    Changed to an area plot

--- altair/vegalite/v2/examples/layered_histogram.py
+++ altair/vegalite/v2/examples/layered_histogram.py
@@ -20,7 +20,7 @@ df = pd.melt(df, id_vars=df.index.name,
              var_name = 'Experiment',
              value_name='Measurement')
 
-chart = alt.Chart(df).mark_bar(opacity=0.3).encode(
+chart = alt.Chart(df).mark_area(opacity=0.3, interpolate='step').encode(
     x=alt.X('Measurement',bin = alt.BinParams(maxbins=100)),
     y=alt.Y('count(*):Q', stack=None), 
     color=alt.Color('Experiment', 
@@ -28,4 +28,4 @@ chart = alt.Chart(df).mark_bar(opacity=0.3).encode(
                                            '#008000', 
                                            '#ff0000'])
                    )
-).configure_bar(binSpacing=0)
+)
