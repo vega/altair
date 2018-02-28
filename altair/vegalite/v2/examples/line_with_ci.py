5f422640c6daf96ab917d72a2f73f7d3eed144ed
Eitan Lees <eitanlees@gmail.com>
2018-02-28 10:30:44 -0500

    Line chart with Confidence Interval Band Example

---
+++ altair/vegalite/v2/examples/line_with_ci.py
@@ -0,0 +1,23 @@
+"""
+Line chart with Confidence Interval Band
+========================================
+This example shows how to make a line chart with a confidence interval Band
+"""
+
+import altair as alt
+from vega_datasets import data
+
+cars = data.cars()
+
+line = alt.Chart(cars).mark_line().encode(
+    x='Year', 
+    y='mean(Miles_per_Gallon)'
+)
+
+confidence_interval = alt.Chart(cars).mark_area(opacity=0.3).encode(
+    x='Year', 
+    y=alt.Y('ci0(Miles_per_Gallon)', axis=alt.Axis(title='Miles/Gallon')),
+    y2='ci1(Miles_per_Gallon)'
+)
+
+chart = confidence_interval + line
