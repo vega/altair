a8d08fdd52a6a45c85119e976ef1afeb33edad77
Jake VanderPlas <jakevdp@gmail.com>
2018-03-18 02:10:41 -0700

    TST: add functionality to skip tests that require internet connection

--- altair/utils/_py3k_compat.py
+++ altair/utils/_py3k_compat.py
@@ -4,11 +4,15 @@ PY2 = sys.version_info[0] == 2
 PY3 = not PY2
 
 if PY2:
+    from urllib2 import URLError, HTTPError, urlopen
+    from urllib import urlretrieve
     string_types = basestring,
     integer_types = (int, long)
     import BaseHTTPServer as server
     from StringIO import StringIO as IO
 else:
+    from urllib.error import URLError, HTTPError
+    from urllib.request import urlopen, urlretrieve
     string_types = str,
     integer_types = int,
     from http import server
