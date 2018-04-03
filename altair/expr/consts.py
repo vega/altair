ae75f72856d52454e369d3169fc0afc69ecdd4b0
Jake VanderPlas <jakevdp@gmail.com>
2018-04-03 10:23:25 -0400

    MAINT: remove unused imports & clean up code

--- altair/expr/consts.py
+++ altair/expr/consts.py
@@ -15,11 +15,13 @@ CONST_LISTING = {
 
 NAME_MAP = {}
 
+
 def _populate_namespace():
     globals_ = globals()
     for name, doc in CONST_LISTING.items():
         py_name = NAME_MAP.get(name, name)
         globals_[py_name] = ConstExpression(name, doc)
         yield py_name
+
 
 __all__ = list(_populate_namespace())
