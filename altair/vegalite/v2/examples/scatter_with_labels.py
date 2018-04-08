b4e691a31c71cbabe158b0abd219fa79d79d341f
palewire <ben.welsh@gmail.com>
2018-04-08 14:43:57 -0700

    Fix

--- altair/vegalite/v2/examples/scatter_with_labels.py
+++ altair/vegalite/v2/examples/scatter_with_labels.py
@@ -10,7 +10,7 @@ import pandas as pd
 data = pd.DataFrame({
     'x': [1, 3, 5, 7, 9],
     'y': [1, 3, 5, 7, 9],
-    'label':['A', 'B', 'C', 'D', 'E']
+    'label': ['A', 'B', 'C', 'D', 'E']
 })
 
 bars = alt.Chart(data).mark_point().encode(
