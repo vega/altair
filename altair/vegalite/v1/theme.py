631cc1970f3b217a649b4150ddfab5756ef057eb
Jake VanderPlas <jakevdp@gmail.com>
2018-04-10 09:23:15 -0700

    ENH: add theme registries for vegalite v1 & v2

---
+++ altair/vegalite/v1/theme.py
@@ -0,0 +1,17 @@
+"""Tools for enabling and registering chart themes"""
+
+from ...utils import PluginRegistry
+
+# The entry point group that can be used by other packages to declare other
+# renderers that will be auto-detected. Explicit registration is also
+# allowed by the PluginRegistery API.
+ENTRY_POINT_GROUP = 'altair.vegalite.v1.theme'  # type: str
+
+class ThemeRegistry(PluginRegistry[dict]):
+    pass
+    
+theme = ThemeRegistry(entry_point_group=ENTRY_POINT_GROUP)
+
+theme.register('default', {"width": 400, "height": 300})
+theme.register('none', {})
+theme.enable('default')
