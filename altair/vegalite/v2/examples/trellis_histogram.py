6b17edd9e6bfd6839cdd752c0e0f960dfb43cd0c
palewire <ben.welsh@gmail.com>
2018-04-23 14:32:23 -0700

    Updated data import style in trellis histogram example

--- altair/vegalite/v2/examples/trellis_histogram.py
+++ altair/vegalite/v2/examples/trellis_histogram.py
@@ -5,10 +5,11 @@ This example shows how to make a basic trellis histogram.
 https://vega.github.io/vega-lite/examples/trellis_bar_histogram.html
 """
 import altair as alt
+from vega_datasets import data
 
-cars = alt.load_dataset('cars')
+source = data.cars()
 
-alt.Chart(cars).mark_bar().encode(
+alt.Chart(source).mark_bar().encode(
     alt.X("Horsepower:Q", bin=True),
     y='count()',
     row='Origin'
