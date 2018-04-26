4017f2347cda2fd7efb42458c1dd3de164e41107
Jake VanderPlas <jakevdp@gmail.com>
2018-04-25 19:35:14 -0700

    TST: make test agnostic of dict order

--- altair/vegalite/v2/tests/test_renderers.py
+++ altair/vegalite/v2/tests/test_renderers.py
@@ -10,9 +10,11 @@ def test_colab_renderer_embed_options():
     with alt.renderers.enable_context('colab', embed_options=dict(actions=False)):
         bundle = chart._repr_mimebundle_(None, None)
         html = bundle['text/html']
-        assert 'embed_opt = {"actions": false, "mode": "vega-lite"}' in html
+        assert ('embed_opt = {"actions": false, "mode": "vega-lite"}' in html or
+                'embed_opt = {"mode": "vega-lite", "actions": false}' in html)
 
     with alt.renderers.enable_context('colab', embed_options=dict(actions=True)):
         bundle = chart._repr_mimebundle_(None, None)
         html = bundle['text/html']
-        assert 'embed_opt = {"actions": true, "mode": "vega-lite"}' in html
+        assert ('embed_opt = {"actions": true, "mode": "vega-lite"}' in html or
+                'embed_opt = {"mode": "vega-lite", "actions": true}' in html)
