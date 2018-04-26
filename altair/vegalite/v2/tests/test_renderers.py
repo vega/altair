c15d9b4969b6171cd50c33b13b0f233bfb044b06
Jake VanderPlas <jakevdp@gmail.com>
2018-04-25 19:14:26 -0700

    ENH: pass renderer embed_options to colab html

---
+++ altair/vegalite/v2/tests/test_renderers.py
@@ -0,0 +1,18 @@
+"""Tests of various renderers"""
+
+import altair as alt
+
+
+def test_colab_renderer_embed_options():
+    """Test that embed_options in renderer metadata are correctly manifest in html"""
+    chart = alt.Chart('data.csv').mark_point()
+
+    with alt.renderers.enable_context('colab', embed_options=dict(actions=False)):
+        bundle = chart._repr_mimebundle_(None, None)
+        html = bundle['text/html']
+        assert 'embed_opt = {"actions": false, "mode": "vega-lite"}' in html
+
+    with alt.renderers.enable_context('colab', embed_options=dict(actions=True)):
+        bundle = chart._repr_mimebundle_(None, None)
+        html = bundle['text/html']
+        assert 'embed_opt = {"actions": true, "mode": "vega-lite"}' in html
