f9d4bf7f6e085aa2e21229e5a6f5b141d5822ddd
palewire <ben.welsh@gmail.com>
2018-07-16 14:59:16 -0700

    Standardized stacked bar examples

--- altair/vegalite/v2/examples/stacked_bar_chart.py
+++ altair/vegalite/v2/examples/stacked_bar_chart.py
@@ -2,24 +2,16 @@
 Stacked Bar Chart
 -----------------
 
-This example shows how to make a stacked bar chart of the weather type in Seattle from 2012 to 2015 by month.
+This is an example of a stacked bar chart using data which contains crop yields over different regions and different years in the 1930s.
 """
 # category: bar charts
 import altair as alt
 from vega_datasets import data
 
-weather = data.seattle_weather()
+barley = data.barley()
 
-alt.Chart(weather).mark_bar().encode(
-    alt.Color('weather:N',
-        legend=alt.Legend(title='Weather type'),
-        scale=alt.Scale(
-            domain=['sun', 'fog', 'drizzle', 'rain', 'snow'],
-            range=['#e7ba42', '#c7c7c7', '#aec7e8', '#1f77b4', '#9467bd']
-        ),
-    ),
-    alt.X('month(date):N',
-        axis=alt.Axis(title='Month of the Year'),
-    ),
-    y='count()',
+alt.Chart(barley).mark_bar().encode(
+    x='variety',
+    y='sum(yield)',
+    color='site'
 )
