555fc82c97a5ad300713b9b10e7488d52fbbb282
Eitan Lees <eitanlees@gmail.com>
2018-08-25 15:44:38 -0400

    DOC: add tooltips to weather heatmap example

--- altair/vegalite/v2/examples/weather_heatmap.py
+++ altair/vegalite/v2/examples/weather_heatmap.py
@@ -1,6 +1,6 @@
 """
 Weather Heatmap
---------------
+---------------
 This example shows the 2010 daily max temperature (F) in Seattle, WA.
 """
 # category: other charts
@@ -12,5 +12,7 @@ df = data.seattle_temps.url
 alt.Chart(df).mark_rect().encode(
     alt.X('date:O', timeUnit='date') ,
     alt.Y('date:O', timeUnit='month'), 
-    alt.Color('temp:Q', aggregate='max')
+    alt.Color('temp:Q', aggregate='max'),
+    tooltip=[alt.Tooltip('date:T', timeUnit='monthdate', title='Date'),
+             alt.Tooltip('temp:Q', aggregate='max', title='Max Temp')]
 )
