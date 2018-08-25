be993dd270a7a3c747155fc51769585d6216bb17
Eitan Lees <eitanlees@gmail.com>
2018-08-25 12:59:06 -0400

    DOC: add weather heatmap example

---
+++ altair/vegalite/v2/examples/weather_heatmap.py
@@ -0,0 +1,16 @@
+"""
+Weather Heatmap
+--------------
+This example shows the 2010 daily max temperature (F) in Seattle, WA.
+"""
+# category: other charts
+import altair as alt
+from vega_datasets import data
+
+df = data.seattle_temps.url
+
+alt.Chart(df).mark_rect().encode(
+    alt.X('date:O', timeUnit='date') ,
+    alt.Y('date:O', timeUnit='month'), 
+    alt.Color('temp:Q', aggregate='max')
+)
