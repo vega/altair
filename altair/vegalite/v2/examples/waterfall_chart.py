2cd01fb8f149830d5b277259e6e2cc020fde06f6
Fernando Irarrazaval <fbirarra@uc.cl>
2018-09-13 18:35:49 -0400

    Added a original dataset transformed in pandas using cumsum()

--- altair/vegalite/v2/examples/waterfall_chart.py
+++ altair/vegalite/v2/examples/waterfall_chart.py
@@ -9,30 +9,41 @@ import pandas as pd
 from altair import datum
 
 data = pd.DataFrame([
-    {"costs": "Advertising costs", "start": 0, "end": 3},
-    {"costs": "Fixed costs", "start": 3, "end": 8},
-    {"costs": "Other costs", "start": 8, "end": 10},
-    {"costs": "Total costs", "start": 0, "end": 10}
+    {"Fixed costs": 5,
+     "Advertising costs": 3,
+     "Other costs": 2},
 ])
 
-waterfall_bars = alt.Chart(data).mark_bar(size=100).encode(
+data_waterfall = data.melt()
+
+data_waterfall['start'] = data_waterfall.value.cumsum().shift().fillna(0)
+data_waterfall['end'] = data_waterfall.value.cumsum()
+data_waterfall.drop(['value'], axis=1, inplace=True)
+
+data_waterfall = data_waterfall.append(
+                    {'variable': 'Total costs',
+                     'start': 0,
+                     'end': max(data_waterfall['end'])
+                     }, ignore_index=True)
+
+waterfall_bars = alt.Chart(data_waterfall).mark_bar(size=100).encode(
     y2=alt.Y2('end:Q'),
     y=alt.Y('start:Q', axis=alt.Axis(grid=False)),
-    x=alt.X('costs:N')
+    x=alt.X('variable:N')
 )
 
-ticks = alt.Chart(data).mark_tick(
+ticks = alt.Chart(data_waterfall).mark_tick(
     color="grey",
     size=150,
     xOffset=100
 ).encode(
     y='end:Q',
-    x='costs:N'
+    x='variable:N'
 ).transform_filter(
-    datum.costs != "Total costs"
+    datum.variable != "Total costs"
 )
 
-(waterfall_bars + ticks).configure_axisX(
+(waterfall_bars+ticks).configure_axisX(
     labelAngle=0
 ).properties(
     width=500
