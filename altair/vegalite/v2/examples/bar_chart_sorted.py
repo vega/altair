a8117eb29597cc9d01c6584603ceebc4ebca8876
Eitan Lees <eitanlees@gmail.com>
2018-09-29 16:10:22 -0400

    DOC: add sorted bar chart example

---
+++ altair/vegalite/v2/examples/bar_chart_sorted.py
@@ -0,0 +1,18 @@
+"""
+Simple Sorted Bar Chart
+============================
+This example shows a basic sorted bar chart.
+"""
+# category: bar charts
+import altair as alt
+import pandas as pd
+
+data = pd.DataFrame({
+    'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+    'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
+})
+
+alt.Chart(data).mark_bar().encode(
+    x=alt.X('a', sort=alt.EncodingSortField(field='b', op='sum', order='descending')),
+    y='b'
+)
