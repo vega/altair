4ccf28e0f4cea240fd11ca573d262aac6bf4d2c3
Ben Welsh <ben.welsh@gmail.com>
2018-10-13 09:59:53 -0700

    Create filled_step_chart.py

---
+++ altair/vegalite/v2/examples/filled_step_chart.py
@@ -0,0 +1,20 @@
+"""
+Filled Step Chart
+-----------------
+This example shows Google's stock price over time as a step chart with its area filled in. 
+"""
+# category: line charts
+import altair as alt
+from vega_datasets import data
+
+chart = alt.Chart(stocks).encode(
+    x='date',
+    y='price'
+).transform_filter(
+    alt.datum.symbol == 'GOOG'
+)
+
+line = chart.mark_line(color="darkblue")
+area = chart.mark_area(color="lightblue")
+
+(line + area).configure_mark(interpolate='step-after')
