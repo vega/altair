7a8f5db648de2df2b407b33f9ead4df39252ebd8
palewire <ben.welsh@gmail.com>
2018-11-21 09:55:12 -0800

    Regroup error bars chart in the example gallery

--- altair/examples/error_bars_with_ci.py
+++ altair/examples/error_bars_with_ci.py
@@ -1,11 +1,9 @@
 """
 Error Bars showing Confidence Interval
 ======================================
-This example shows how to show error bars using confidence intervals.
-The confidence intervals are computed internally in vega by
-a non-parametric `bootstrap of the mean <https://github.com/vega/vega-statistics/blob/master/src/bootstrapCI.js>`_.
+This example shows how to show error bars using confidence intervals. The confidence intervals are computed internally in vega by a non-parametric `bootstrap of the mean <https://github.com/vega/vega-statistics/blob/master/src/bootstrapCI.js>`_.
 """
-# category: bar charts
+# category: other charts
 import altair as alt
 from vega_datasets import data
 
