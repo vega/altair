6af9fa2c869a28d11dea11f8c5675fcc3552e10f
eleijonmarck <eric.leijonmarck@gmail.com>
2019-01-31 18:15:51 +0300

    review updates

--- altair/examples/top_k_items.py
+++ altair/examples/top_k_items.py
@@ -1,28 +1,28 @@
 """
 Top K Items
--------------
+-----------
 This example shows how to use the window and transformation filter to display
 the Top items of a long list of items in decreasing order.
 The sorting of the x-axis is needed for vega-lite and for the example does
-not do anything since we already have a unique value (in this case a movie).
+not do anything since we already have a unique value.
 Here we sort the top 10 highest ranking movies of IMDB.
 """
 # category: case studies
 import altair as alt
-
 from vega_datasets import data
-movies = data.movies()
+
+source = data.movies.url
 
 # Top 10 movies by IMBD ratings
 alt.Chart(
-    movies,
+    source,
 ).mark_bar().encode(
     x=alt.X(
         'Title:N', 
         sort=alt.EncodingSortField(field="IMDB_Rating", op="mean", order='descending')
     ),
-    y=alt.Y('IMDB_Rating'),
-    color=alt.Color('IMDB_Rating')
+    y=alt.Y('IMDB_Rating:Q'),
+    color=alt.Color('IMDB_Rating:Q')
     
 ).transform_window(
     rank='rank(IMDB_Rating)',
