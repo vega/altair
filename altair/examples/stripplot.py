69992daf4d3620bb6c923b7ded366cb2d89086eb
ilia_timofeev <ilia.timofeev@gmail.com>
2019-05-20 18:36:07 +0300

    data.url+ formatting

--- altair/examples/stripplot.py
+++ altair/examples/stripplot.py
@@ -7,33 +7,33 @@ This example shows how to make a Stripplot.
 import altair as alt
 from vega_datasets import data
 
-source = data.movies()
+source = data.movies.url
 
-stripplot = (
-    alt.Chart(source, width=40)
-    .mark_circle(size=8)
-    .encode(
-        alt.X(
-            shorthand="jitter:Q",
-            title=None,
-            axis=alt.Axis(values=[0], ticks=True, grid=False, labels=False),
-            scale=alt.Scale(),
+stripplot =  alt.Chart(source, width=40).mark_circle(size=8).encode(
+    x=alt.X(
+        'jitter:Q',
+        title=None,
+        axis=alt.Axis(values=[0], ticks=True, grid=False, labels=False),
+        scale=alt.Scale(),
+    ),
+    y=alt.Y('IMDB_Rating:Q'),
+    color=alt.Color('Major_Genre:N', legend=None),
+    column=alt.Column(
+        'Major_Genre:N',
+        header=alt.Header(
+            labelAngle=-90,
+            titleOrient='top',
+            labelOrient='bottom',
+            labelAlign='right',
+            labelPadding=3,
         ),
-        alt.Y("IMDB_Rating"),
-        alt.Color("Major_Genre", legend=None),
-        alt.Column(
-            "Major_Genre",
-            header=alt.Header(
-                labelAngle=-90,
-                titleOrient="top",
-                labelOrient="bottom",
-                labelAlign="right",
-                labelPadding=3,
-            ),
-        ),
-    )
-    .transform_calculate(jitter="pow(random()-0.5,3)")
-    .configure_facet(spacing=0)
-    .configure_view(stroke=None)
+    ),
+).transform_calculate(
+    jitter='pow(random()-0.5,3)'
+).configure_facet(
+    spacing=0
+).configure_view(
+    stroke=None
 )
+
 stripplot
