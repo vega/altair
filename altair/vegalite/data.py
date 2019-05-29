c8fca565e7bf2e91255040a7e329c93b511001c3
Jake VanderPlas <jakevdp@google.com>
2019-05-28 22:42:31 -0700

    BUG: respect data transformer for chart.serve() and chart.save()

--- altair/vegalite/data.py
+++ altair/vegalite/data.py
@@ -3,13 +3,24 @@ from toolz.curried import curry, pipe
 from ..utils.core import sanitize_dataframe
 from ..utils.data import (
     MaxRowsError, limit_rows, sample, to_csv, to_json, to_values,
-    check_data_type, DataTransformerRegistry
+    check_data_type
 )
+from ..utils.data import DataTransformerRegistry as _DataTransformerRegistry
 
 
 @curry
 def default_data_transformer(data, max_rows=5000):
     return pipe(data, limit_rows(max_rows=max_rows), to_values)
+
+
+class DataTransformerRegistry(_DataTransformerRegistry):
+    def disable_max_rows(self):
+        """Disable the MaxRowsError."""
+        options = self.options
+        if self.active == 'default':
+            options = options.copy()
+            options['max_rows'] = None
+        return self.enable(**options)
 
 
 __all__ = (
