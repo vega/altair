3e1dc158673102377de6e08d79b0e8a4eff9dbe6
Jake VanderPlas <jakevdp@google.com>
2019-07-23 21:45:22 -0700

    MAINT: schemapi: refactor from_dict to be more general

--- tools/schemapi/schemapi.py
+++ tools/schemapi/schemapi.py
@@ -365,8 +365,7 @@ class SchemaBase(object):
         if _wrapper_classes is None:
             _wrapper_classes = cls._default_wrapper_classes()
         converter = _FromDict(_wrapper_classes)
-        return converter.from_dict(constructor=cls, root=cls,
-                                   schema=cls._schema, dct=dct)
+        return converter.from_dict(dct, cls)
 
     @classmethod
     def from_json(cls, json_string, validate=True, **kwargs):
@@ -483,28 +482,36 @@ class _FromDict(object):
         else:
             raise ValueError("Must have 0 or 1 positional argument.")
 
-    def _get_constructor(self, schema):
-        # TODO: do something more than simply selecting the last match?
-        hash_ = self.hash_schema(schema)
-        matches = self.class_dict[hash_]
-        return matches[-1] if matches else self._passthrough
-
-    def from_dict(self, constructor, root, schema, dct):
+    def from_dict(self, dct, cls=None, schema=None, rootschema=None):
         """Construct an object from a dict representation"""
-        # TODO: introspect lists, objects, etc. when they don't have a wrapper.
-        #       could do this by passing the schema rather than cls.
-        schema = root.resolve_references(schema)
+        if schema is None:
+            if cls is None:
+                raise ValueError("Must specify either cls or schema.")
+            elif not (isinstance(cls, type) and issubclass(cls, SchemaBase)):
+                raise ValueError("schema must be specified if cls is not a SchemaBase object.")
+            schema = cls._schema
+            rootschema = rootschema or cls._rootschema
+        rootschema = rootschema or schema
+
+        if cls is None:
+            # TODO: do something more than simply selecting the last match?
+            matches = self.class_dict[self.hash_schema(schema)]
+            cls = matches[-1] if matches else self._passthrough
+        schema = _resolve_references(schema, rootschema)
 
         if 'anyOf' in schema or 'oneOf' in schema:
             schemas = schema.get('anyOf', []) + schema.get('oneOf', [])
-            for this_schema in schemas:
-                this_constructor = self._get_constructor(this_schema)
+            for possible_schema in schemas:
+                resolver = jsonschema.RefResolver.from_schema(rootschema)
                 try:
-                    root.validate(dct, this_schema)
+                    jsonschema.validate(dct, possible_schema, resolver=resolver)
                 except jsonschema.ValidationError:
                     continue
                 else:
-                    return self.from_dict(this_constructor, root, this_schema, dct)
+                    return self.from_dict(dct,
+                        schema=possible_schema,
+                        rootschema=rootschema,
+                    )
 
         if isinstance(dct, dict):
             # TODO: handle schemas for additionalProperties/patternProperties
@@ -512,20 +519,19 @@ class _FromDict(object):
             kwds = {}
             for key, val in dct.items():
                 if key in props:
-                    prop_constructor = self._get_constructor(props[key])
-                    val = self.from_dict(prop_constructor, root, props[key], val)
+                    val = self.from_dict(val,
+                        schema=props[key],
+                        rootschema=rootschema
+                    )
                 kwds[key] = val
-            return constructor(**kwds)
+            return cls(**kwds)
 
         elif isinstance(dct, list):
-            if 'items' in schema:
-                item_schema = schema['items']
-                item_constructor = self._get_constructor(item_schema)
-            else:
-                item_schema = {}
-                item_constructor = self._passthrough
-            dct = [self.from_dict(item_constructor, root, item_schema, val)
-                   for val in dct]
-            return constructor(dct)
+            item_schema = schema.get('items', {})
+            dct = [self.from_dict(val,
+                       schema=item_schema,
+                       rootschema=rootschema
+                   ) for val in dct]
+            return cls(dct)
         else:
-            return constructor(dct)
+            return cls(dct)
