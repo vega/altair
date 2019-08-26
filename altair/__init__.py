a6ef0aa8df90010799f123843b658911822c3adb
Philipp Kats <casyfill@gmail.com>
2019-08-26 17:53:13 -0400

    raise on plotting attempt

--- altair/__init__.py
+++ altair/__init__.py
@@ -8,3 +8,7 @@ def load_ipython_extension(ipython):
     from ._magics import vega, vegalite
     ipython.register_magic_function(vega, 'cell')
     ipython.register_magic_function(vegalite, 'cell')
+
+
+def plot(*args, **kwargs):
+    raise NotImplementedError('''To use Altair as an engine for pandas, install altair-pandas package:\n\npip install git+https://github.com/altair-viz/altair_pandas''')  
