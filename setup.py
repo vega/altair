49006ba179f445eed35544fbe06ac7fd1a7ecd87
Jake VanderPlas <jakevdp@google.com>
2019-10-18 21:01:15 -0700

    MAINT: support Python 3.8

--- setup.py
+++ setup.py
@@ -98,5 +98,7 @@ setup(name=NAME,
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'],
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+      ],
      )
