a699bb725c31c4c8197cdf56c86db716d8b80d7a
Eitan Lees <eitanlees@gmail.com>
2019-12-10 13:36:52 -0500

    DOC: refactor some examples which use EncodingSortField

--- altair/examples/bar_chart_sorted.py
+++ altair/examples/bar_chart_sorted.py
@@ -11,12 +11,5 @@ source = data.barley()
 
 alt.Chart(source).mark_bar().encode(
     x='sum(yield):Q',
-    y=alt.Y(
-        'site:N',
-        sort=alt.EncodingSortField(
-            field="yield",  # The field to use for the sort
-            op="sum",  # The operation to run on the field prior to sorting
-            order="descending"  # The order to sort in
-        )
-    )
+    y=alt.Y('site:N', sort='-x')
 )
