9a9a33e42b13233b61405a56db790fd2c72f0159
Jake VanderPlas <jakevdp@google.com>
2020-02-25 20:40:20 -0800

    Cleanup: use deprecation utils for all deprecation warnings

--- altair/datasets.py
+++ altair/datasets.py
@@ -1,23 +1,22 @@
-"""Datasets for use with Altair
+"""Datasets for use with Altair (deprecated)
 
 The functions here are merely thin wrappers for data access routines in the
 vega_datasets package.
 """
-import warnings
-
-MSG = ("load_dataset is deprecated. "
-       "Please use the vega_datasets package instead.")
+from altair.utils.deprecation import deprecated
 
 
+@deprecated("load_dataset is deprecated. " "Use the vega_datasets package instead.")
 def load_dataset(name):
     """Load a dataset by name as a pandas.DataFrame."""
-    warnings.warn(MSG, DeprecationWarning)
     import vega_datasets
+
     return vega_datasets.data(name)
 
 
+@deprecated("load_dataset is deprecated. " "Use the vega_datasets package instead.")
 def list_datasets():
     """List the available datasets."""
-    warnings.warn(MSG, DeprecationWarning)
     import vega_datasets
+
     return vega_datasets.data.list_datasets()
