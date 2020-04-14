5c5931537297a62bf056160de8eec58e0cc13464
palewire <ben.welsh@gmail.com>
2020-04-13 20:44:22 -0700

    Created a cumulative sum line chart example

---
+++ altair/examples/line_chart_with_cumsum.py
@@ -0,0 +1,23 @@
+"""
+Line Chart with Cumulative Sum
+------------------------------
+This chart creates a simple line chart from the cumulative sum of a fields.
+"""
+# category: line charts
+import altair as alt
+from vega_datasets import data
+
+source = data.wheat()
+
+alt.Chart(source).mark_line().transform_window(
+    # Sort the data chronologically in this case
+    sort=[{'field': 'year'}],
+    # Include all previous records before the current record and none after
+    frame=[None, 0],
+    # What we want to add up
+    cumulative_wheat='sum(wheat)',
+).encode(
+    x='year:O',
+    # Plot the calculated field created by the transformation
+    y='cumulative_wheat:Q'
+)
