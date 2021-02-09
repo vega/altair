e975554cccd6c79b466841206a0c060b42834503
Joel Ostblom <joelostblom@users.noreply.github.com>
2021-02-09 09:36:12 -0800

    Correct boxplot example to reflect title

--- altair/examples/boxplot.py
+++ altair/examples/boxplot.py
@@ -1,9 +1,10 @@
 """
-Box Plot with Min/Max Whiskers
+Boxplot with Min/Max Whiskers
 ------------------------------
-This example shows how to make a basic box plot using US Population data from 2000.  
-Note that the default value of the `extent` property is 1.5, which can be used to 
-produce in the standard modified boxplot.
+This example shows how to make a boxplot using US Population data from 2000.  
+Note that the default value of the `extent` property is 1.5,
+which represents the convention of extending the whiskers
+to the furthest points within 1.5 * IQR from the first and third quartile.
 """
 # category: other charts
 import altair as alt
@@ -11,7 +12,7 @@ from vega_datasets import data
 
 source = data.population.url
 
-alt.Chart(source).mark_boxplot().encode(
+alt.Chart(source).mark_boxplot(extent='min-max').encode(
     x='age:O',
     y='people:Q'
 )
