24f1fe1213001fd58f6173b4c877cf4732858fdb
Ben Welsh <ben.welsh@gmail.com>
2022-06-30 19:55:03 -0700

    Move image tooltip example to interactive charts section

--- altair/examples/image_tooltip.py
+++ altair/examples/image_tooltip.py
@@ -5,7 +5,7 @@ This example shows how to render images in tooltips.
 Either URLs or local file paths can be used to reference
 the images.
 """
-# category: other charts
+# category: interactive charts
 
 import altair as alt
 import pandas as pd
