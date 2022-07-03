c510c88398b6a0ce4536524266ca645411e7334d
Ben Welsh <ben.welsh@gmail.com>
2022-07-03 07:12:51 -0700

    Update ranged_dot_plot.py

--- altair/examples/ranged_dot_plot.py
+++ altair/examples/ranged_dot_plot.py
@@ -1,7 +1,7 @@
 """
 Ranged Dot Plot
 -----------------
-This example shows a ranged dot plot that uses 'layer' to convey changing life expectancy for the five most populous countries (between 1955 and 2000).
+This example shows a ranged dot plot to convey changing life expectancy for the five most populous countries (between 1955 and 2000).
 """
 # category: other charts
 import altair as alt
@@ -9,7 +9,7 @@ from vega_datasets import data
 
 source = data.countries.url
 
-chart = alt.layer(
+chart = alt.Chart(
     data=source
 ).transform_filter(
     filter={"field": 'country',
@@ -19,13 +19,13 @@ chart = alt.layer(
             "oneOf": [1955, 2000]}
 )
 
-chart += alt.Chart().mark_line(color='#db646f').encode(
+line = chart.mark_line(color='#db646f').encode(
     x='life_expect:Q',
     y='country:N',
     detail='country:N'
 )
 # Add points for life expectancy in 1955 & 2000
-chart += alt.Chart().mark_point(
+points = chart.mark_point(
     size=100,
     opacity=1,
     filled=True
@@ -34,10 +34,10 @@ chart += alt.Chart().mark_point(
     y='country:N',
     color=alt.Color('year:O',
         scale=alt.Scale(
-            domain=['1955', '2000'],
+            domain=[1955, 2000],
             range=['#e6959c', '#911a24']
         )
     )
 ).interactive()
 
-chart
+(line + points)
