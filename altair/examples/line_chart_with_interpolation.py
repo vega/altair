ca16acaf346815ffcc1b9baf41425ed2beac97b5
palewire <b@palewi.re>
2022-07-03 07:34:21 -0700

    Add line chart with interpolation

---
+++ altair/examples/line_chart_with_interpolation.py
@@ -0,0 +1,15 @@
+"""
+Line Chart with Interpolation
+-----------------------------
+This chart shows a line chart with the path interpolated. A full list of interpolation methods is available `in the documentation <https://altair-viz.github.io/user_guide/generated/core/altair.LineConfig.html?highlight=interpolate#altair-lineconfig>`_.
+"""
+# category: line charts
+import altair as alt
+from vega_datasets import data
+
+source = data.wheat()
+
+alt.Chart(source).mark_line(interpolate="monotone").encode(
+    x="year:O",
+    y="wheat:Q"
+).properties(width=600)
