7054e9ba01971eed47f7b2a718e48db0347d4355
Ben Welsh <ben.welsh@gmail.com>
2022-07-05 07:48:08 -0700

    Update binned_heatmap.py

--- altair/examples/binned_heatmap.py
+++ altair/examples/binned_heatmap.py
@@ -3,7 +3,7 @@ Binned Heatmap
 --------------
 This example shows how to make a heatmap from binned quantitative data.
 """
-# category: heatmaps
+# category: tables
 import altair as alt
 from vega_datasets import data
 
