626f04dd9db215f459d57cfcbec02f4b74d792f8
Christopher Davis <christopherjdavis@gmail.com>
2022-09-21 07:02:33 -0700

    Update vega to v5.22.1

--- altair/vega/v5/schema/__init__.py
+++ altair/vega/v5/schema/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 from .core import *
 
-SCHEMA_VERSION = 'v5.10.0'
-SCHEMA_URL = 'https://vega.github.io/schema/vega/v5.10.0.json'
+SCHEMA_VERSION = 'v5.22.1'
+SCHEMA_URL = 'https://vega.github.io/schema/vega/v5.22.1.json'
