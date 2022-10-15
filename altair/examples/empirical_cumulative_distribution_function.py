2aeb714e9750b3c3027f0a065c36bb647f23f655
Stefan Binder <binder.stefan@hispeed.ch>
2022-10-15 13:53:04 +0000

    Add gallery example for empirical cumulative distribution function

---
+++ altair/examples/empirical_cumulative_distribution_function.py
@@ -0,0 +1,15 @@
+"""
+Empirical Cumulative Distribution Function
+------------------------------------------
+This example shows an empirical cumulative distribution function.
+"""
+# category: line charts
+import altair as alt
+from vega_datasets import data
+
+source = data.movies.url
+
+alt.Chart(source).transform_window(
+    ecdf="cume_dist()",
+    sort=[{"field": "IMDB_Rating"}],
+).mark_line(interpolate="step-after").encode(x="IMDB_Rating:Q", y="ecdf:Q")
