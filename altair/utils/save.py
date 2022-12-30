8c04fd71df6e65a082e9b01808860d9bacc2d0e8
Jon Mease <jonmmease@gmail.com>
2022-12-30 11:03:32 -0500

    Test vl-convert engine in chart.save test (#2784)
    * Explicitly pass engine argument through save function
    
    * Update test_save to test both altair_saver and vl-convert engiens
    
    * flake8

--- altair/utils/save.py
+++ altair/utils/save.py
@@ -26,6 +26,7 @@ def save(
     json_kwds=None,
     webdriver=None,
     scale_factor=1,
+    engine=None,
     **kwargs,
 ):
     """Save a chart to file in a variety of formats
@@ -61,6 +62,8 @@ def save(
         Webdriver to use for png or svg output
     scale_factor : float (optional)
         scale_factor to use to change size/resolution of png or svg output
+    engine: string {'vl-convert', 'altair_saver'}
+        the conversion engine to use for 'png', 'svg', and 'pdf' formats
     **kwargs :
         additional kwargs passed to spec_to_mimebundle.
     """
@@ -122,6 +125,7 @@ def save(
             vegaembed_version=vegaembed_version,
             webdriver=webdriver,
             scale_factor=scale_factor,
+            engine=engine,
             **kwargs,
         )
         if format == "png":
@@ -131,4 +135,4 @@ def save(
         else:
             write_file_or_filename(fp, mimebundle["image/svg+xml"], mode="w")
     else:
-        raise ValueError("unrecognized format: '{}'".format(format))
+        raise ValueError("Unsupported format: '{}'".format(format))
