2b4213dfe4d1f366e81e041fa2ce06a621de0558
Stefan Binder <binder_stefan@outlook.com>
2022-12-31 11:19:29 +0100

    Remove update_subtraits as not used anywhere. Presumably from an older implementation based on traitlets? (#2787)

--- altair/utils/core.py
+++ altair/utils/core.py
@@ -557,34 +557,6 @@ def use_signature(Obj):
     return decorate
 
 
-def update_subtraits(obj, attrs, **kwargs):
-    """Recursively update sub-traits without overwriting other traits"""
-    # TODO: infer keywords from args
-    if not kwargs:
-        return obj
-
-    # obj can be a SchemaBase object or a dict
-    if obj is Undefined:
-        obj = dct = {}
-    elif isinstance(obj, SchemaBase):
-        dct = obj._kwds
-    else:
-        dct = obj
-
-    if isinstance(attrs, str):
-        attrs = (attrs,)
-
-    if len(attrs) == 0:
-        dct.update(kwargs)
-    else:
-        attr = attrs[0]
-        trait = dct.get(attr, Undefined)
-        if trait is Undefined:
-            trait = dct[attr] = {}
-        dct[attr] = update_subtraits(trait, attrs[1:], **kwargs)
-    return obj
-
-
 def update_nested(original, update, copy=False):
     """Update nested dictionaries
 
