8e37ff3593f8a29eeea463297a11ee6fb14617f5
Christopher Davis <82750240+ChristopherDavisUCI@users.noreply.github.com>
2023-01-04 07:50:40 -0600

    Update tests/vegalite/v5/tests/test_alias.py
    Co-authored-by: Joel Ostblom <joelostblom@users.noreply.github.com>

--- tests/vegalite/v5/tests/test_alias.py
+++ tests/vegalite/v5/tests/test_alias.py
@@ -4,7 +4,7 @@ import altair.vegalite.v5 as alt
 
 
 def test_aliases():
-    """We define `Bin` to be an alias `BinParams`,  and similarly `Impute` for `ImputeParams` and `Title` for `TitleParams`.  Here we ensure that `Bin`, `Impute`, and `Title` are not already defined in `core.py` or `channels.py`."""
+    """Ensure that any aliases defined in `api.py` aren't colliding with names already defined in `core.py` or `channels.py`."""
     for alias in ["Bin", "Impute", "Title"]:
         getattr(alt, alias)
 
