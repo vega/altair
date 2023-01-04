62c02eb36c8735b8447c9e648ad527c4ecbfc96a
Christopher Davis <82750240+ChristopherDavisUCI@users.noreply.github.com>
2023-01-04 09:35:19 -0600

    Update tests/vegalite/v5/tests/test_alias.py
    Co-authored-by: Mattijn van Hoek <mattijn@gmail.com>

--- tests/vegalite/v5/tests/test_alias.py
+++ tests/vegalite/v5/tests/test_alias.py
@@ -6,10 +6,16 @@ import altair.vegalite.v5 as alt
 def test_aliases():
     """Ensure that any aliases defined in `api.py` aren't colliding with names already defined in `core.py` or `channels.py`."""
     for alias in ["Bin", "Impute", "Title"]:
-        getattr(alt, alias)
+        # this test pass if the alias can resolve to its real name
+        try:
+            getattr(alt, alias)
+        except AttributeError as e:
+            assert False, f"cannot resolve '{alias}':, {e}"
 
+        # this test fails if the alias match a colliding name in core
         with pytest.raises(AttributeError):
             getattr(alt.core, alias)
 
+        # this test fails if the alias match a colliding name in channels
         with pytest.raises(AttributeError):
             getattr(alt.channels, alias)
