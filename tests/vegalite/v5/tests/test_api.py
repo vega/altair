291bcfc376b6954a09e2c4292b0b7ba13868b108
Jon Mease <jonmmease@gmail.com>
2023-01-08 11:13:54 -0500

    Add inline argument to chart.save() for html export (#2807)
    * Add inline argument to chart.save()
    
    This includes the Vega/Vega-Lite/Vega-Embed JavaScript source in the exported html file so that it works offline.  The logic is ported from altair_saver.
    
    * Only override the `template` argument if `inline=True`
    
    This way it's still possible to pass in custom templates
    
    * Add 'pdf' to `format` argument docstring
    
    * Another missed pdf format
    
    * check for @5 instead of @5.2.0 for forward compatibility
    
    Co-authored-by: Mattijn van Hoek <mattijn@gmail.com>
    
    * Don't search for closing quote to allow for later versions in the future
    
    Co-authored-by: Mattijn van Hoek <mattijn@gmail.com>

--- tests/vegalite/v5/tests/test_api.py
+++ tests/vegalite/v5/tests/test_api.py
@@ -335,6 +335,23 @@ def test_save(format, engine, basic_chart):
             os.remove(fp)
 
 
+@pytest.mark.parametrize("inline", [False, True])
+def test_save_html(basic_chart, inline):
+    out = io.StringIO()
+    basic_chart.save(out, format="html", inline=inline)
+    out.seek(0)
+    content = out.read()
+
+    assert content.startswith("<!DOCTYPE html>")
+
+    if inline:
+        assert '<script type="text/javascript">' in content
+    else:
+        assert 'src="https://cdn.jsdelivr.net/npm/vega@5' in content
+        assert 'src="https://cdn.jsdelivr.net/npm/vega-lite@5' in content
+        assert 'src="https://cdn.jsdelivr.net/npm/vega-embed@6' in content
+
+
 def test_facet_basic():
     # wrapped facet
     chart1 = (
