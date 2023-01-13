14b355d8bdc49140ce8680fe2e9c0383972be1d7
Christopher Davis <christopherjdavis@gmail.com>
2023-01-13 05:32:48 -0800

    Add tests related to parameters

--- tests/vegalite/v5/tests/test_params.py
+++ tests/vegalite/v5/tests/test_params.py
@@ -2,6 +2,11 @@
 
 import pandas as pd
 
+import warnings
+import pytest
+
+import re
+
 import altair.vegalite.v5 as alt
 
 
@@ -41,11 +46,11 @@ def test_param_expr():
     inner_var = height_var / 2
     tick_var = (3 * height_var) / 4
 
-    inner_str = inner_var._to_expr().replace(" ", "")
-    inner_ans = "(height/2)"
+    inner_str = inner_var._to_expr()
+    inner_ans = "(height / 2)"
 
-    tick_str = tick_var._to_expr().replace(" ", "")
-    tick_ans = "((3*height)/4)"
+    tick_str = tick_var._to_expr()
+    tick_ans = "((3 * height) / 4)"
 
     assert height_var._to_expr() == "height"
     assert inner_str == inner_ans
@@ -62,8 +67,101 @@ def test_param_expr():
     c = (c1 + c2 + c3).add_params(height_var)
 
     dct = c.to_dict()
-
-    ans_set = {v._to_expr() for v in [height_var, inner_var, tick_var]}
     expr_set = {d["mark"]["size"]["expr"] for d in dct["layer"]}
 
-    assert ans_set == expr_set
+    assert "height" in expr_set
+    assert "(height / 2)" in expr_set
+    assert "((3 * height) / 4)" in expr_set
+
+
+def test_selection_deprecation():
+    # We use the `warnings.catch_warnings` context so pytest will also report deprecation warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+
+        # new syntax
+        alt.selection_point()
+        alt.selection_interval()
+
+        # this v4 syntax is deprecated
+        with pytest.warns(alt.utils.deprecation.AltairDeprecationWarning):
+            alt.selection_single()
+        with pytest.warns(alt.utils.deprecation.AltairDeprecationWarning):
+            alt.selection_multi()
+
+        # new syntax
+        brush = alt.selection_interval()
+        c = alt.Chart().mark_point()
+        c.add_params(brush)
+
+        # this v4 syntax is deprecated
+        brush = alt.selection_interval()
+        c = alt.Chart().mark_point()
+        with pytest.warns(alt.utils.deprecation.AltairDeprecationWarning):
+            c.add_selection(brush)
+
+
+def test_parameter_naming():
+    # test explicit naming
+    prm = alt.param(name="some_name")
+    assert prm.param.name == "some_name"
+
+    # test automatic naming which has the form such as param_5
+    prm0, prm1, prm2 = [alt.param() for _ in range(3)]
+
+    res = re.match("param_([0-9]+)", prm0.param.name)
+
+    assert res
+
+    num = int(res[1])
+    assert prm1.param.name == f"param_{num+1}"
+    assert prm2.param.name == f"param_{num+2}"
+
+
+def test_selection_expression():
+    data = pd.DataFrame([{"a": "A", "b": 28}])
+
+    sel = alt.selection_point(fields=["b"])
+    se = sel.b | 300
+
+    assert isinstance(se, alt.SelectionExpression)
+    assert isinstance(se.expr, alt.expr.core.Expression)
+
+    c = (
+        alt.Chart(data)
+        .mark_point()
+        .encode(
+            x="a:N",
+            y="b:Q",
+            size=alt.value(se),
+        )
+        .add_params(sel)
+    )
+
+    dct = c.to_dict()
+    expr_str = str(se.expr)
+
+    assert dct["encoding"]["size"]["value"]["expr"] == expr_str
+
+
+def test_selection_condition():
+    sel = alt.selection_point(empty=False)
+
+    c = (
+        alt.Chart()
+        .mark_point()
+        .encode(size=alt.condition(sel, alt.value(100), alt.value(10)))
+        .add_params(sel)
+    )
+
+    dct = c.to_dict()
+
+    param_name = sel.param.name
+
+    cond = dct["encoding"]["size"]["condition"]
+
+    assert cond["value"] == 100
+    assert cond["param"] == param_name
+
+    # The else condition
+    assert dct["encoding"]["size"]["value"] == 10
