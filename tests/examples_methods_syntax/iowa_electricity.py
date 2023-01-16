4b352a91d0aeacd249bc3da6ba4dca5ce03d40cd
Ben Welsh <ben.welsh@gmail.com>
2023-01-16 13:48:26 -0800

    Update iowa_electricity.py

--- tests/examples_methods_syntax/iowa_electricity.py
+++ tests/examples_methods_syntax/iowa_electricity.py
@@ -9,7 +9,10 @@ from vega_datasets import data
 
 source = data.iowa_electricity()
 
-alt.Chart(source, title="Iowa's renewable energy boom").mark_area().encode(
+alt.Chart(
+    source,
+    title=alt.Title("Iowa's green energy boom", subtitle="A growing share of the state's energy has come from renewable sources")
+).mark_area().encode(
     alt.X("year:T").title("Year"),
     alt.Y("net_generation:Q")
         .title("Share of net generation")
