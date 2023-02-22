783ffc2f007e416a4c823059f3a9385412fd5df2
Mattijn van Hoek <mattijn@gmail.com>
2023-02-22 21:41:07 +0100

    Include `alt.ExprRef` capabilities in `alt.expr()` (#2886)
    * define _ExprType to include ExprRef as function
    
    * add expr tests for function style
    
    * fix linting
    
    * more linting
    
    * moved _ExprType class to init of altair.expr
    
    * Add imports on the same line
    
    ---------
    
    Co-authored-by: Joel Ostblom <joelostblom@users.noreply.github.com>

--- altair/expr/__init__.py
+++ altair/expr/__init__.py
@@ -3,3 +3,15 @@
 from .core import datum, Expression
 from .funcs import *
 from .consts import *
+from ..vegalite.v5.schema.core import ExprRef as _ExprRef
+
+
+class _ExprType:
+    def __init__(self, expr):
+        vars(self).update(expr)
+
+    def __call__(self, expr, **kwargs):
+        return _ExprRef(expr, **kwargs)
+
+
+expr = _ExprType(globals())
