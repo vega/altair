729f199a75e86ea37b474d24d2c73c783b103c47
Stefan Binder <binder_stefan@outlook.com>
2023-03-04 12:17:50 +0000

    Remove decorator.py and test_decorator.py from tools as they are not used

--- tools/schemapi/__init__.py
+++ tools/schemapi/__init__.py
@@ -2,8 +2,7 @@
 schemapi: tools for generating Python APIs from JSON schemas
 """
 from .schemapi import SchemaBase, Undefined
-from .decorator import schemaclass
 from .utils import SchemaInfo
 
 
-__all__ = ("SchemaBase", "Undefined", "schemaclass", "SchemaInfo")
+__all__ = ("SchemaBase", "Undefined", "SchemaInfo")
