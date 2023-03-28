cb5ab758ec62d60357a132b98119cf39bbfc9761
Joel Ostblom <joelostblom@users.noreply.github.com>
2023-03-28 13:36:34 -0700

    Simplify syntax (#2991)

--- tests/examples_arguments_syntax/bar_chart_with_labels.py
+++ tests/examples_arguments_syntax/bar_chart_with_labels.py
@@ -7,19 +7,12 @@ This example shows a basic horizontal bar chart with labels created with Altair.
 import altair as alt
 from vega_datasets import data
 
+
 source = data.wheat()
 
-bars = alt.Chart(source).mark_bar().encode(
-    x='wheat:Q',
-    y="year:O"
+base = alt.Chart(source).encode(
+    x='wheat',
+    y="year:O",
+    text='wheat'
 )
-
-text = bars.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
-).encode(
-    text='wheat:Q'
-)
-
-(bars + text).properties(height=900)
+base.mark_bar() + base.mark_text(align='left', dx=2)
