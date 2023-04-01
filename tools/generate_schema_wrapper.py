7d8084b1fd0853d08f35997b33af937889833015
mattijn <mattijn@gmail.com>
2023-04-01 09:24:25 +0200

    update files to ruff: noqa

--- tools/generate_schema_wrapper.py
+++ tools/generate_schema_wrapper.py
@@ -581,7 +581,7 @@ def vegalite_main(skip_download=False):
         outfile = join(schemapath, "__init__.py")
         print("Writing {}".format(outfile))
         with open(outfile, "w", encoding="utf8") as f:
-            f.write("# flake8: noqa\n")
+            f.write("# ruff: noqa\n")
             f.write("from .core import *\nfrom .channels import *\n")
             f.write(
                 "SCHEMA_VERSION = {!r}\n" "".format(SCHEMA_VERSION[library][version])
