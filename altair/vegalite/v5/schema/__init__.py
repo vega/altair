acdd1cec17f9d531a4fdf7e5257a5338a03c8f8a
Stefan Binder <binder_stefan@outlook.com>
2023-04-17 18:48:46 +0000

    Update to VL 5.7.1

--- altair/vegalite/v5/schema/__init__.py
+++ altair/vegalite/v5/schema/__init__.py
@@ -1,5 +1,5 @@
 # ruff: noqa
 from .core import *
 from .channels import *
-SCHEMA_VERSION = 'v5.7.0'
-SCHEMA_URL = 'https://vega.github.io/schema/vega-lite/v5.7.0.json'
+SCHEMA_VERSION = 'v5.7.1'
+SCHEMA_URL = 'https://vega.github.io/schema/vega-lite/v5.7.1.json'
