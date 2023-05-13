5571f39904cba4acac7c4d248788cd95e8a44a8b
Stefan Binder <binder_stefan@outlook.com>
2023-05-13 21:04:12 +0200

    Use altairplot Sphinx directive of sphinxext_altair package (#3056)
    * Use altairplot Sphinx directive of sphinxext_altair package
    
    * Use sphinxext_altair package from pypi
    
    * Add note about sphinxext-altair package in release 5

--- doc/conf.py
+++ doc/conf.py
@@ -12,8 +12,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
+import sys
 from datetime import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -36,7 +36,7 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.githubpages",
     "numpydoc.numpydoc",
-    "sphinxext.altairplot",
+    "sphinxext_altair.altairplot",
     "sphinxext.altairgallery",
     "sphinxext.schematable",
     "sphinx_copybutton",
