b5657b75be84f983c6d1d0039a76277973903843
Jon Mease <jonmmease@gmail.com>
2023-07-20 12:23:26 -0400

    Input Altair Chart, traitlets for selections and params

--- altair/widget/__init__.py
+++ altair/widget/__init__.py
@@ -1,11 +1,86 @@
 import anywidget  # type: ignore
 import traitlets
 import pathlib
+from dataclasses import dataclass
+from typing import Any, Dict, List
+
+import altair as alt
+from altair.utils._vegafusion_data import using_vegafusion
+from altair.vegalite.v5.schema.core import TopLevelSpec
 
 _here = pathlib.Path(__file__).parent
 
 
+@dataclass
+class Param:
+    name: str
+    value: Any
+
+
+@dataclass
+class SelectionParam:
+    name: str
+    value: Dict[str, Any]
+    store: List[Dict[str, Any]]
+
+
 class ChartWidget(anywidget.AnyWidget):
     _esm = _here / "static" / "index.js"
-    spec = traitlets.Unicode().tag(sync=True)
-    selection = traitlets.Dict().tag(sync=True)
+    _css = r"""
+    .vega-embed {
+        overflow: visible;
+    }
+    """
+
+    chart = traitlets.Instance(TopLevelSpec)
+    spec = traitlets.Dict().tag(sync=True)
+    selections = traitlets.Dict()
+    params = traitlets.Dict()
+
+    _selection_watches = traitlets.List().tag(sync=True)
+    _selections = traitlets.Dict().tag(sync=True)
+
+    _param_watches = traitlets.List().tag(sync=True)
+    _params = traitlets.Dict().tag(sync=True)
+
+    @traitlets.observe("chart")
+    def change_chart(self, change):
+        new_chart: TopLevelSpec = change.new
+
+        params = getattr(new_chart, "params", [])
+        selection_watches = []
+        param_watches = []
+        if params is not alt.Undefined:
+            for param in new_chart.params:
+                select = getattr(param, "select", alt.Undefined)
+                if select != alt.Undefined:
+                    selection_watches.append(param.name)
+                else:
+                    param_watches.append(param.name)
+
+        # Update properties all together
+        with self.hold_sync():
+            if using_vegafusion():
+                self.spec = new_chart.to_dict(format="vega")
+            else:
+                self.spec = new_chart.to_dict()
+            self._selection_watches = selection_watches
+            self._param_watches = param_watches
+
+    @traitlets.observe("_selections")
+    def change_selections(self, change):
+        new_selections = dict()
+        for selection_name, selection_dict in change.new.items():
+            new_selections[selection_name] = SelectionParam(
+                name=selection_name, **selection_dict
+            )
+        self.selections = new_selections
+
+    @traitlets.observe("_params")
+    def change_params(self, change):
+        new_params = dict()
+        for param_name, param_dict in change.new.items():
+            new_params[param_name] = Param(
+                name=param_name, **param_dict
+            )
+        self.params = new_params
--- widget/src/index.js
+++ widget/src/index.js
@@ -1,11 +1,48 @@
 import embed from "vega-embed";
 
 export async function render({ model, el }) {
-    let spec = JSON.parse(model.get("spec"));
-    let api = await embed(el, spec);
-    api.view.addSignalListener(spec.params[0].name, (_, update) => {
-        console.log(update);
-        model.set("selection", update);
+    let finalize;
+
+    const reembed = async () => {
+        if (finalize != null) {
+          finalize();
+        }
+
+        let spec = model.get("spec");
+        let api = await embed(el, spec);
+        finalize = api.finalize;
+
+        const selectionWatches = model.get("_selection_watches");
+        const initialSelections = {};
+        for (const selectionName of selectionWatches) {
+            api.view.addSignalListener(selectionName, (_, value) => {
+                const newSelections = JSON.parse(JSON.stringify(model.get("_selections"))) || {};
+                const store = JSON.parse(JSON.stringify(api.view.data(`${selectionName}_store`)));
+
+                newSelections[selectionName] = {value, store};
+                model.set("_selections", newSelections);
+                model.save_changes();
+            });
+            initialSelections[selectionName] = {value: {}, store: []}
+        }
+        model.set("_selections", initialSelections);
+
+        const paramWatches = model.get("_param_watches");
+        const initialParams = {};
+        for (const paramName of paramWatches) {
+            api.view.addSignalListener(paramName, (_, value) => {
+                const newParams = JSON.parse(JSON.stringify(model.get("_params"))) || {};
+                newParams[paramName] = {value};
+                model.set("_params", newParams);
+                model.save_changes();
+            });
+            initialParams[paramName] = {value: api.view.signal(paramName)}
+        }
+        model.set("_params", initialParams);
+
         model.save_changes();
-    });
+    }
+
+    model.on('change:spec', reembed);
+    await reembed();
 }
