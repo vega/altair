12478f64bf0255fc338200ef0055e20faa36a69d
Nick Crews <nicholas.b.crews@gmail.com>
2023-08-18 20:07:50 -0800

    docs: examples: remove connected_scatterplot (#3159)

--- tests/examples_methods_syntax/line_custom_order.py
+++ tests/examples_methods_syntax/line_custom_order.py
@@ -1,7 +1,7 @@
 """
 Line Chart with Custom Order
 ----------------------------
-By default, the line’s path (order of points in the line) is
+By default, the line's path (order of points in the line) is
 determined by data values on the temporal/ordinal field.
 However, a field can be mapped to the order channel for a custom path.
 
@@ -10,6 +10,9 @@ gasoline price and average miles driven per capita we use
 order channel to sort the points in the line by time field (year).
 The earliest year (1956) is one endpoint and the latest year (2010)
 is the other endpoint.
+
+This is based on Hannah Fairfield's article 'Driving Shifts Into Reverse'.
+See https://archive.nytimes.com/www.nytimes.com/imagepages/2010/05/02/business/02metrics.html.
 """
 # category: line charts
 import altair as alt
