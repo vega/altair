b77edb694f0ffa0cdf3c6810ea0add2da7ffb5db
Jon Mease <jonmmease@gmail.com>
2023-08-21 15:27:43 -0400

    Use importlib.metadata.version

--- altair/utils/_importers.py
+++ altair/utils/_importers.py
@@ -1,63 +1,66 @@
 from types import ModuleType
 from packaging.version import Version
+from importlib.metadata import version as importlib_version
 
 
 def import_vegafusion() -> ModuleType:
-    min_vf_version = "1.4.0"
+    min_version = "1.4.0"
     try:
+        version = importlib_version("vegafusion")
+        if Version(version) < Version(min_version):
+            raise ImportError(
+                f"The vegafusion package must be version {min_version} or greater. "
+                f"Found version {version}"
+            )
         import vegafusion as vf  # type: ignore
 
-        if Version(vf.__version__) < Version(min_vf_version):
-            raise ImportError(
-                f"The vegafusion package must be version {min_vf_version} or greater. "
-                f"Found version {vf.__version__}"
-            )
         return vf
     except ImportError as err:
         raise ImportError(
             'The "vegafusion" data transformer and chart.transformed_data feature requires\n'
-            f"version {min_vf_version} or greater of the 'vegafusion-python-embed' and 'vegafusion' packages.\n"
+            f"version {min_version} or greater of the 'vegafusion-python-embed' and 'vegafusion' packages.\n"
             "These can be installed with pip using:\n"
-            f'    pip install "vegafusion[embed]>={min_vf_version}"\n'
+            f'    pip install "vegafusion[embed]>={min_version}"\n'
             "Or with conda using:\n"
-            f'    conda install -c conda-forge "vegafusion-python-embed>={min_vf_version}" '
-            f'"vegafusion>={min_vf_version}"\n\n'
+            f'    conda install -c conda-forge "vegafusion-python-embed>={min_version}" '
+            f'"vegafusion>={min_version}"\n\n'
             f"ImportError: {err.args[0]}"
         ) from err
 
 
 def import_vl_convert() -> ModuleType:
-    min_vlc_version = "0.12.0"
+    min_version = "0.12.0"
     try:
+        version = importlib_version("vl-convert-python")
+        if Version(version) < Version(min_version):
+            raise ImportError(
+                f"The vl-convert-python package must be version {min_version} or greater. "
+                f"Found version {version}"
+            )
         import vl_convert as vlc
 
-        if Version(vlc.__version__) < Version(min_vlc_version):
-            raise ImportError(
-                f"The vl-convert-python package must be version {min_vlc_version} or greater. "
-                f"Found version {vlc.__version__}"
-            )
         return vlc
     except ImportError as err:
         raise ImportError(
             f"The vl-convert Vega-Lite compiler and image export feature requires\n"
-            f"version {min_vlc_version} or greater of the 'vl-convert-python' package. \n"
+            f"version {min_version} or greater of the 'vl-convert-python' package. \n"
             f"This can be installed with pip using:\n"
-            f'   pip install "vl-convert-python>={min_vlc_version}"\n'
+            f'   pip install "vl-convert-python>={min_version}"\n'
             "or conda:\n"
-            f'   conda install -c conda-forge "vl-convert-python>={min_vlc_version}"\n\n'
+            f'   conda install -c conda-forge "vl-convert-python>={min_version}"\n\n'
             f"ImportError: {err.args[0]}"
         ) from err
 
 
 def import_pyarrow_interchange() -> ModuleType:
-    min_pa_version = "11.0.0"
+    min_version = "11.0.0"
     try:
-        import pyarrow as pa
+        version = importlib_version("pyarrow")
 
-        if Version(pa.__version__) < Version(min_pa_version):
+        if Version(version) < Version(min_version):
             raise ImportError(
-                f"The pyarrow package must be version {min_pa_version} or greater. "
-                f"Found version {pa.__version__}"
+                f"The pyarrow package must be version {min_version} or greater. "
+                f"Found version {version}"
             )
         import pyarrow.interchange as pi
 
@@ -65,11 +68,11 @@ def import_pyarrow_interchange() -> ModuleType:
     except ImportError as err:
         raise ImportError(
             f"Usage of the DataFrame Interchange Protocol requires\n"
-            f"version {min_pa_version} or greater of the pyarrow package. \n"
+            f"version {min_version} or greater of the pyarrow package. \n"
             f"This can be installed with pip using:\n"
-            f'   pip install "pyarrow>={min_pa_version}"\n'
+            f'   pip install "pyarrow>={min_version}"\n'
             "or conda:\n"
-            f'   conda install -c conda-forge "pyarrow>={min_pa_version}"\n\n'
+            f'   conda install -c conda-forge "pyarrow>={min_version}"\n\n'
             f"ImportError: {err.args[0]}"
         ) from err
 
