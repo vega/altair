f69cb81f081f14327fe5c197223170403533c058
Nick Crews <nicholas.b.crews@gmail.com>
2023-08-29 18:25:40 -0800

    example: Use facet in anscombe_plot
    See https://github.com/altair-viz/altair/issues/3178

--- tests/examples_arguments_syntax/anscombe_plot.py
+++ tests/examples_arguments_syntax/anscombe_plot.py
@@ -2,7 +2,14 @@
 Anscombe's Quartet
 ------------------
 
-This example shows how to use the column channel to make a trellis plot. Anscombe's Quartet is a famous dataset constructed by Francis Anscombe. Common summary statistics are identical for each subset of the data, despite the subsets having vastly different characteristics.
+`Anscombe's Quartet <https://en.wikipedia.org/wiki/Anscombe%27s_quartet>`_
+is a famous dataset constructed by Francis Anscombe.
+It is made of 4 different subsets of data.
+Each subset has very different characteristics, even though common summary
+statistics such as mean and variance are identical.
+
+This example shows how to make a faceted plot, with each facet
+showing a different subset of the data.
 """
 # category: case studies
 import altair as alt
@@ -11,9 +18,9 @@ from vega_datasets import data
 source = data.anscombe()
 
 alt.Chart(source).mark_circle().encode(
-    alt.X('X', scale=alt.Scale(zero=False)),
-    alt.Y('Y', scale=alt.Scale(zero=False)),
-    alt.Facet('Series', columns=2),
+    alt.X("X", scale=alt.Scale(zero=False)),
+    alt.Y("Y", scale=alt.Scale(zero=False)),
+    alt.Facet("Series", columns=2),
 ).properties(
     width=180,
     height=180,
