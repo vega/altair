2a508fd464aef459c317f9e74e9d8d22ecd9a79f
Joel Ostblom <joel.ostblom@gmail.com>
2023-10-03 07:38:46 -0700

    Update to 5.15.1

--- altair/vegalite/v5/schema/__init__.py
+++ altair/vegalite/v5/schema/__init__.py
@@ -1,5 +1,5 @@
 # ruff: noqa
 from .core import *
 from .channels import *
-SCHEMA_VERSION = 'v5.15.0'
-SCHEMA_URL = 'https://vega.github.io/schema/vega-lite/v5.15.0.json'
+SCHEMA_VERSION = 'v5.15.1'
+SCHEMA_URL = 'https://vega.github.io/schema/vega-lite/v5.15.1.json'
