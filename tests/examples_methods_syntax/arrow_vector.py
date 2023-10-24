261430c8918dfc3bf7d922e39762862ed11195ca
Weng, Chia-Ling <75072960+ChiaLingWeng@users.noreply.github.com>
2023-10-25 00:40:54 +0800

    Fix language as argument syntax example

--- tests/examples_methods_syntax/arrow_vector.py
+++ tests/examples_methods_syntax/arrow_vector.py
@@ -1,10 +1,10 @@
 """
 Arrow Vector
 ------------------------------
-This example shows a basic vector plot use point mark ``triangle`` as shape,
+This example shows a basic vector plot using point mark ``triangle`` as shape,
 other shape options can be found in :ref:`Point Mark<user-guide-point-marks>`.
 """
-# category: case studies
+# category: line charts
 import altair as alt
 import numpy as np
 import pandas as pd
@@ -12,23 +12,23 @@ import pandas as pd
 vector1 = [0, 0, 3, -1]
 vector2 = [0, 0, 2, 3]
 
-v = pd.DataFrame([vector1, vector2], columns=['x1','y1','x2','y2'])
+v = pd.DataFrame([vector1, vector2], columns=["x1","y1","x2","y2"])
 
 # calculate the vector
-v['x_'] = v['x2'] - v["x1"]
-v['y_'] = v['y2'] - v["y1"]
+v["x_"] = v["x2"] - v["x1"]
+v["y_"] = v["y2"] - v["y1"]
 
 # calculate the angle between current vector and the default point mark direction (0,1)
 # dot product = (x_,y_) dot (0,1) = y_
-v["norm"] = np.sqrt(v['x_']**2 + v['y_']**2)
-v["theta"] = np.degrees(np.arccos(v['y_']/v["norm"]))
+v["norm"] = np.sqrt(v["x_"]**2 + v["y_"]**2)
+v["theta"] = np.degrees(np.arccos(v["y_"]/v["norm"]))
 
 lines = alt.Chart(v).mark_line().encode(
     alt.X("x1")
-    .title('x')
+    .title("x")
     .scale(domain=(-4, 4)),
     alt.Y("y1")
-    .title('y')
+    .title("y")
     .scale(domain=(-4, 4)),
     alt.X2("x2"),
     alt.Y2("y2")
@@ -40,7 +40,7 @@ wedge = alt.Chart(v).mark_point(shape="triangle", filled=True).encode(
     alt.Angle("theta")
     .scale(domain=[0, 360]),
     alt.SizeValue(300),
-    alt.ColorValue('#000000')
+    alt.ColorValue("#000000")
 )
 
 lines + wedge
