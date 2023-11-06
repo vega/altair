1243e73be097e391c84ac57cbeae22a12a7eb28e
Stefan Binder <binder_stefan@outlook.com>
2023-11-06 22:18:16 +0100

    Type hint utils/save.py and utils/mimebundle.py (#3248)
    * Type hint mimebundle.py
    
    * Type hint save.py
    
    * Fix type hint for py38

--- altair/utils/mimebundle.py
+++ altair/utils/mimebundle.py
@@ -1,3 +1,5 @@
+from typing import Literal, Optional, Union, cast, Tuple
+
 from .deprecation import AltairDeprecationWarning
 from .html import spec_to_html
 from ._importers import import_vl_convert
@@ -6,15 +8,15 @@ import warnings
 
 
 def spec_to_mimebundle(
-    spec,
-    format,
-    mode=None,
-    vega_version=None,
-    vegaembed_version=None,
-    vegalite_version=None,
-    engine=None,
+    spec: dict,
+    format: Literal["html", "json", "png", "svg", "pdf", "vega", "vega-lite"],
+    mode: Optional[Literal["vega-lite"]] = None,
+    vega_version: Optional[str] = None,
+    vegaembed_version: Optional[str] = None,
+    vegalite_version: Optional[str] = None,
+    engine: Optional[Literal["vl-convert", "altair_saver"]] = None,
     **kwargs,
-):
+) -> Union[dict, Tuple[dict, dict]]:
     """Convert a vega-lite specification to a mimebundle
 
     The mimebundle type is controlled by the ``format`` argument, which can be
@@ -54,18 +56,20 @@ def spec_to_mimebundle(
     if mode != "vega-lite":
         raise ValueError("mode must be 'vega-lite'")
 
+    internal_mode: Literal["vega-lite", "vega"] = mode
     if using_vegafusion():
         spec = compile_with_vegafusion(spec)
-        mode = "vega"
+        internal_mode = "vega"
 
     if format in ["png", "svg", "pdf", "vega"]:
+        format = cast(Literal["png", "svg", "pdf", "vega"], format)
         return _spec_to_mimebundle_with_engine(
-            spec, format, mode, engine=engine, **kwargs
+            spec, format, internal_mode, engine=engine, **kwargs
         )
     if format == "html":
         html = spec_to_html(
             spec,
-            mode=mode,
+            mode=internal_mode,
             vega_version=vega_version,
             vegaembed_version=vegaembed_version,
             vegalite_version=vegalite_version,
@@ -84,7 +88,12 @@ def spec_to_mimebundle(
     )
 
 
-def _spec_to_mimebundle_with_engine(spec, format, mode, **kwargs):
+def _spec_to_mimebundle_with_engine(
+    spec: dict,
+    format: Literal["png", "svg", "pdf", "vega"],
+    mode: Literal["vega-lite", "vega"],
+    **kwargs,
+) -> Union[dict, Tuple[dict, dict]]:
     """Helper for Vega-Lite to mimebundle conversions that require an engine
 
     Parameters
@@ -183,7 +192,10 @@ def _spec_to_mimebundle_with_engine(spec, format, mode, **kwargs):
         )
 
 
-def _validate_normalize_engine(engine, format):
+def _validate_normalize_engine(
+    engine: Optional[Literal["vl-convert", "altair_saver"]],
+    format: Literal["png", "svg", "pdf", "vega"],
+) -> str:
     """Helper to validate and normalize the user-provided engine
 
     engine : {None, 'vl-convert', 'altair_saver'}
