19b370d50429f0455752a01dbfa522d6b58fa844
Mattijn van Hoek <mattijn@gmail.com>
2023-11-10 16:54:31 +0100

    data as url

--- tests/examples_methods_syntax/interactive_aggregation.py
+++ tests/examples_methods_syntax/interactive_aggregation.py
@@ -11,7 +11,7 @@ represents the aggregation. Adapted from an example by @dwootton.
 import altair as alt
 from vega_datasets import data
 
-source = data.movies()
+source = data.movies.url
 
 slider = alt.binding_range(min=0, max=10, step=0.1)
 threshold = alt.param(name="threshold", value=5, bind=slider)
@@ -27,7 +27,7 @@ alt.layer(
     alt.Chart(source).mark_circle().encode(
         x=alt.X("IMDB_Rating:Q").bin(maxbins=10),
         y=alt.Y("Rotten_Tomatoes_Rating:Q").bin(maxbins=10),
-        size=alt.Size("count()").scale(domain=[0,160])
+        size=alt.Size("count():Q").scale(domain=[0,160])
     ).transform_filter(
         alt.datum["IMDB_Rating"] < threshold
     ),
