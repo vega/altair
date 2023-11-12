1172f189dfa09d0662309e322427bbccd2b82102
Joel Ostblom <joelostblom@users.noreply.github.com>
2023-11-11 16:32:35 -0800

    Simplify code a bit

--- tests/examples_arguments_syntax/line_chart_with_arrows.py
+++ tests/examples_arguments_syntax/line_chart_with_arrows.py
@@ -1,63 +1,65 @@
 """
 Line Chart with Arrows
 ----------------------
-This example shows a simple line chart with arrow annotations.
-It utilizes a Unicode character for quick arrow insertion, 
-and employs the triangle :ref:`point mark <user-guide-point-marks>` for customizable arrowheads.
-The Unicode method offers ease of use, while the triangle point mark provides greater flexibility.
+This example shows a simple line chart with two types of arrow annotations.
+The Unicode character approach is simpler, 
+while the line plus triangle :ref:`point mark <user-guide-point-marks>` allows for greater flexibility,
+such as customizable arrowheads.
 """
 # category: line charts
+import altair as alt
 import numpy as np
 import pandas as pd
-import altair as alt
+
 
 x = np.linspace(1,7)
-y = np.sin(x)
-
 data = pd.DataFrame({
-            "x": x,
-            "y": y,
-        })
+    "x": x,
+    "y": np.sin(x),
+})
 
-alt.layer(
-    alt.Chart(data).mark_line(point=True,tooltip=True).encode(
-        x=alt.X("x").title("x"),
-        y=alt.Y("y").title("y"),
+unicode_arrow_with_text = alt.layer(
+    # Arrow
+    alt.Chart().mark_text(size=60, align="left", baseline="bottom", fontWeight=100, angle=340).encode(
+        x=alt.datum(2.8),
+        y=alt.datum(-0.3),
+        text=alt.datum("🠃")  # Any unicode symbol could be used instead
     ),
-     alt.Chart().mark_text(size=14, align="center", baseline="bottom").encode(
+    # Text
+    alt.Chart().mark_text(size=14, align="center", baseline="bottom").encode(
         x=alt.datum(1.8),
         y=alt.datum(-0.1),
         text=alt.datum("decreasing")
+    )
+)
+
+mark_arrow_with_text = alt.layer(
+    # Arrow line
+    alt.Chart().mark_line(size=2).encode(
+        x=alt.datum(5.4),
+        y=alt.datum(-0.4),
+        x2=alt.datum(5.9),
+        y2=alt.datum(0)
     ),
+    # Arrow head
+    alt.Chart().mark_point(shape="triangle", filled=True, fillOpacity=1).encode(
+        x=alt.datum(5.9),
+        y=alt.datum(0),
+        angle=alt.AngleValue(23),
+        size=alt.SizeValue(100),
+        color=alt.ColorValue("#000000")
+    ),
+    # Text
     alt.Chart().mark_text(size=14, align="center", baseline="bottom").encode(
         x=alt.datum(4.7),
         y=alt.datum(-0.3),
         text=alt.datum("increasing")
     ),
-    # unicode arrow
-    alt.Chart().mark_text(size=60, align="left", baseline="bottom", fontWeight=100, angle=340).encode(
-        x=alt.datum(2.8),
-        y=alt.datum(-0.3),
-        text=alt.datum("🠃")
-    ),
-    # arrow line
-    alt.Chart().mark_line(size=2).encode(
-        x=alt.datum(5.4),
-        y=alt.datum(-0.4),
-        x2=alt.datum(6),
-        y2=alt.datum(0)
-    ),
-    # arrow head
-    alt.Chart().mark_point(shape="triangle", filled=True, fillOpacity=1).encode(
-        x=alt.datum(6),
-        y=alt.datum(0),
-        angle=alt.AngleValue(23),
-        size=alt.SizeValue(100),
-        color=alt.ColorValue("#000000")
-    )
 )
 
+line_with_points = alt.Chart(data).mark_line(point=True).encode(
+    x=alt.X("x"),
+    y=alt.Y("y"),
+)
 
-
-
-
+line_with_points + unicode_arrow_with_text + mark_arrow_with_text
