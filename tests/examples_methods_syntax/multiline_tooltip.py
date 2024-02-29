06fba10f9ffc0108f3b407f56d4ce5ad2a2e53bd
Stefan Binder <binder_stefan@outlook.com>
2024-02-29 20:28:54 +0100

    Harmonize code style

--- tests/examples_methods_syntax/multiline_tooltip.py
+++ tests/examples_methods_syntax/multiline_tooltip.py
@@ -15,27 +15,28 @@ import pandas as pd
 import numpy as np
 
 np.random.seed(42)
+columns = ["A", "B", "C"]
 source = pd.DataFrame(
     np.cumsum(np.random.randn(100, 3), 0).round(2),
-    columns=['A', 'B', 'C'], index=pd.RangeIndex(100, name='x')
+    columns=columns, index=pd.RangeIndex(100, name="x")
 )
-source = source.reset_index().melt('x', var_name='category', value_name='y')
+source = source.reset_index().melt("x", var_name="category", value_name="y")
 
 # Create a selection that chooses the nearest point & selects based on x-value
-nearest = alt.selection_point(nearest=True, on='mouseover',
-                        fields=['x'], empty=False)
+nearest = alt.selection_point(nearest=True, on="mouseover",
+                              fields=["x"], empty=False)
 
 # The basic line
-line = alt.Chart(source).mark_line(interpolate='basis').encode(
-    x='x:Q',
-    y='y:Q',
-    color='category:N'
+line = alt.Chart(source).mark_line(interpolate="basis").encode(
+    x="x:Q",
+    y="y:Q",
+    color="category:N"
 )
 
 # Transparent selectors across the chart. This is what tells us
 # the x-value of the cursor
 selectors = alt.Chart(source).mark_point().encode(
-    x='x:Q',
+    x="x:Q",
     opacity=alt.value(0),
 ).add_params(
     nearest
@@ -47,13 +48,13 @@ points = line.mark_point().encode(
 )
 
 # Draw text labels near the points, and highlight based on selection
-text = line.mark_text(align='left', dx=5, dy=-5).encode(
-    text=alt.condition(nearest, 'y:Q', alt.value(' '))
+text = line.mark_text(align="left", dx=5, dy=-5).encode(
+    text=alt.condition(nearest, "y:Q", alt.value(" "))
 )
 
 # Draw a rule at the location of the selection
-rules = alt.Chart(source).mark_rule(color='gray').encode(
-    x='x:Q',
+rules = alt.Chart(source).mark_rule(color="gray").encode(
+    x="x:Q",
 ).transform_filter(
     nearest
 )
--- tests/examples_methods_syntax/multiline_tooltip_standard.py
+++ tests/examples_methods_syntax/multiline_tooltip_standard.py
@@ -14,20 +14,20 @@ np.random.seed(42)
 columns = ["A", "B", "C"]
 source = pd.DataFrame(
     np.cumsum(np.random.randn(100, 3), 0).round(2),
-    columns=columns,
-    index=pd.RangeIndex(100, name="x"),
+    columns=columns, index=pd.RangeIndex(100, name="x"),
 )
 source = source.reset_index().melt("x", var_name="category", value_name="y")
 
-# The basic line
-line = (
-    alt.Chart(source)
-    .mark_line(interpolate="basis")
-    .encode(x="x:Q", y="y:Q", color="category:N")
-)
-
 # Create a selection that chooses the nearest point & selects based on x-value
-nearest = alt.selection_point(nearest=True, on="mouseover", fields=["x"], empty=False)
+nearest = alt.selection_point(nearest=True, on="mouseover", 
+                              fields=["x"], empty=False)
+
+# The basic line
+line = alt.Chart(source).mark_line(interpolate="basis").encode(
+    x="x:Q",
+    y="y:Q",
+    color="category:N"
+)
 
 # Draw points on the line, and highlight based on selection
 points = line.mark_point().encode(
@@ -35,18 +35,20 @@ points = line.mark_point().encode(
 )
 
 # Draw a rule at the location of the selection
-rules = (
-    alt.Chart(source)
-    .transform_pivot("category", value="y", groupby=["x"])
-    .mark_rule(color="gray")
-    .encode(
-        x="x:Q",
-        opacity=alt.condition(nearest, alt.value(0.3), alt.value(0)),
-        tooltip=[alt.Tooltip(c, type="quantitative") for c in columns],
-    )
-    .add_params(nearest)
-)
+rules = alt.Chart(source).transform_pivot(
+    "category",
+    value="y",
+    groupby=["x"]
+).mark_rule(color="gray").encode(
+    x="x:Q",
+    opacity=alt.condition(nearest, alt.value(0.3), alt.value(0)),
+    tooltip=[alt.Tooltip(c, type="quantitative") for c in columns],
+).add_params(nearest)
 
 
 # Put the five layers into a chart and bind the data
-alt.layer(line, points, rules).properties(width=600, height=300)
+alt.layer(
+    line, points, rules
+).properties(
+    width=600, height=300
+)
