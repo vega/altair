87a686c9b42617edab9733baa50876b85757d1b6
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2024-06-15 15:33:58 +0100

    fix: remove debug code

--- tools/generate_schema_wrapper.py
+++ tools/generate_schema_wrapper.py
@@ -14,16 +14,7 @@ from urllib import request
 import m2r
 
 sys.path.insert(0, str(Path.cwd()))
-
-try:
-    from tools.schemapi import codegen, CodeSnippet, SchemaInfo
-except ImportError as err:
-    msg = (
-        f"Unable to find 'tools' in sys.path:\n{sys.path!r}\n\n"
-        f"Cwd: {Path.cwd().as_posix()}"
-    )
-    raise ModuleNotFoundError(msg) from err
-
+from tools.schemapi import codegen, CodeSnippet, SchemaInfo
 from tools.schemapi.utils import (
     get_valid_identifier,
     resolve_references,
