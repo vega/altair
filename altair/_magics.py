2036b9940234cff7b7859fba4c139d0bf8dc5027
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-07-15 18:46:31 +0100

    feat: make pandas and NumPy optional dependencies, don't require PyArrow for plotting with Polars/Modin/cuDF (#3452)
    * feat: use Narwhals to make NumPy and pandas optional, and to support
    Narwhals-compliant libraries natively without PyArrow conversions
    
    * fix ibis-date32 bug too, make sure to only convert to arrow once
    
    * stable api
    
    * update docstring
    
    * fixup
    
    * silence warning for old pandas
    
    * improve skipif condition, simplify
    
    * lint
    
    * explicitly set ibis backend
    
    * use importlib / packaging
    
    * xfail ibis date32 test on windows
    
    * wip
    
    * wip
    
    * wip
    
    * wip date32
    
    * reorder imports
    
    * typing
    
    * bump version
    
    * typo, `strict=False` comments, use consistent typevar name
    
    * lint
    
    * special-case Polars in `.dt.to_string` for Date
    
    * rename test file

--- altair/_magics.py
+++ altair/_magics.py
@@ -9,7 +9,7 @@ import warnings
 
 import IPython
 from IPython.core import magic_arguments
-import pandas as pd
+from narwhals.dependencies import is_pandas_dataframe as _is_pandas_dataframe
 
 from altair.vegalite import v5 as vegalite_v5
 
@@ -39,7 +39,7 @@ def _prepare_data(data, data_transformers):
     """Convert input data to data for use within schema"""
     if data is None or isinstance(data, dict):
         return data
-    elif isinstance(data, pd.DataFrame):
+    elif _is_pandas_dataframe(data):
         if func := data_transformers.get():
             data = func(data)
         return data
