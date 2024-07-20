b9d3fd4480461cdad4ec0317c92441de753b8ad9
Dan Redding <125183946+dangotbanned@users.noreply.github.com>
2024-07-20 13:31:33 +0100

    feat: Reimplement `expr` as a class that is understood by IDEs (#3466)
    * fix: Use absolute imports explicitly
    
    Fixes the mypy issue referenced in `alt.__init__.py`
    
    * refactor: remove dynamic globals manipulation
    
    The results of `_populate_namespace` were only visible in a `IPython` environment
    
    * feat: Reimplement `expr` as a class w/ identicial semantics as the singleton
    
    - all `ConstExpression`s are now read-only properties stored in a metaclass
    - `expr` subclasses `ExprRef`, replacing the `__call__` w/ `__new__`
      - No instances of `expr` are ever created
    - All `FunctionExpression`s are now classmethods
    - Each `Expression` can now be referenced in autocomplete, without needing `IPython`/`Jupyter`
    - Docstrings have been reformatted to be compatible with `sphinx`
    - Broken links in docstrings fixed (e.g. for d3)
    - class-level doc for `expr`, with references & doctest
    
    * fix: use absolute imports in `test_expr`
    
    Previously relied on the dynamic globals
    
    * test(perf): rewrite `expr` tests to run in parallel
    
    Previously 2 tests, now 170+
    
    * test: confirm `expr` constants stay constant
    
    * chore(typing): add ignores not flagged by mypy
    
    * docs: adds `alt.expr` to API Reference
    
    Uses the section heading proposed in https://github.com/vega/altair/pull/3427#discussion_r1659965947
    
    Expands on https://github.com/vega/altair/issues/2904
    
    * test: ensure `test_expr_consts_immutable` pattern works for all versions
    
    * docs: fix typos, regex artifacts, apply some `ruff` `D` rules
    
    * docs: add doc for `_ConstExpressionType`
    
    * test: make `expr` doctest testable
    
    I left this out initially, but by adding static `Parameter` names it is now possible
    
    * fix: re-run `generate-schema-wrapper`
    
    https://github.com/vega/altair/actions/runs/10005909777/job/27657526013?pr=3466
    
    * refactor: Remove `expr` test dependency on constants
    
    Fixes https://github.com/vega/altair/pull/3466#issuecomment-2239264620
    
    * style: remove trailing commas in `@pytest.mark.parametrize`

--- altair/expr/consts.py
+++ altair/expr/consts.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from .core import ConstExpression
-
 
 CONST_LISTING = {
     "NaN": "not a number (same as JavaScript literal NaN)",
@@ -14,16 +12,3 @@ CONST_LISTING = {
     "SQRT2": "the square root of 2 (alias to Math.SQRT1_2)",
     "PI": "the transcendental number pi (alias to Math.PI)",
 }
-
-NAME_MAP: dict[str, str] = {}
-
-
-def _populate_namespace():
-    globals_ = globals()
-    for name, doc in CONST_LISTING.items():
-        py_name = NAME_MAP.get(name, name)
-        globals_[py_name] = ConstExpression(name, doc)
-        yield py_name
-
-
-__all__ = list(_populate_namespace())
