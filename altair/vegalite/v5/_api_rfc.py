07dbcc2ca90fd7c677e4150c5e4cfbed433b2e5b
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2024-07-31 19:02:28 +0100

    fix: botched commit

--- altair/vegalite/v5/_api_rfc.py
+++ altair/vegalite/v5/_api_rfc.py
@@ -10,6 +10,7 @@ Actual runtime dependencies:
 The rest are to define aliases only.
 """
 
+# mypy: ignore-errors
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Dict, Literal, Mapping, Sequence, Union
@@ -25,6 +26,7 @@ from altair.vegalite.v5.schema._typing import (
     MultiTimeUnit_T,
     SingleTimeUnit_T,
     Type_T,
+    Map,
 )
 from altair.vegalite.v5.schema.core import (
     FieldEqualPredicate,
@@ -36,11 +38,13 @@ from altair.vegalite.v5.schema.core import (
     FieldRangePredicate,
     FieldValidPredicate,
 )
+from altair.vegalite.v5.schema import channels
 
 if TYPE_CHECKING:
     from altair.utils.core import DataFrameLike
     from altair.vegalite.v5.schema._typing import AggregateOp_T
     from altair.vegalite.v5.schema.core import Predicate
+
 
 __all__ = ["agg", "field"]
 
@@ -123,6 +127,138 @@ def _one_of_variance(val_1: Any, *rest: OneOfType) -> Sequence[Any]:
             f"{tuple(f'{type(v).__name__}' for v in (val_1, *rest))!r}"
         )
         raise TypeError(msg)
+
+
+class _FieldMeta(type):
+    def __new__(  # type: ignore[misc]
+        cls, shorthand: dict[str, Any] | str, /, data: DataFrameLike | None = None
+    ) -> dict[str, Any]:
+        return _parse(shorthand=shorthand, data=data)
+
+    def argmin(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("argmin", col_name, type)
+
+    def argmax(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("argmax", col_name, type)
+
+    def average(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("average", col_name, type)
+
+    def count(
+        self, col_name: str | None = None, /, type: EncodeType = "Q"
+    ) -> dict[str, Any]:
+        return _parse_aggregate("count", col_name, type)
+
+    def distinct(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("distinct", col_name, type)
+
+    def max(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("max", col_name, type)
+
+    def mean(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("mean", col_name, type)
+
+    def median(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("median", col_name, type)
+
+    def min(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("min", col_name, type)
+
+    def missing(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("missing", col_name, type)
+
+    def product(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("product", col_name, type)
+
+    def q1(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("q1", col_name, type)
+
+    def q3(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("q3", col_name, type)
+
+    def ci0(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("ci0", col_name, type)
+
+    def ci1(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("ci1", col_name, type)
+
+    def stderr(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("stderr", col_name, type)
+
+    def stdev(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("stdev", col_name, type)
+
+    def stdevp(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("stdevp", col_name, type)
+
+    def sum(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("sum", col_name, type)
+
+    def valid(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("valid", col_name, type)
+
+    def values(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("values", col_name, type)
+
+    def variance(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("variance", col_name, type)
+
+    def variancep(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("variancep", col_name, type)
+
+    def exponential(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("exponential", col_name, type)
+
+    def exponentialb(
+        self, col_name: str | None = None, /, type: EncodeType = None
+    ) -> dict[str, Any]:
+        return _parse_aggregate("exponentialb", col_name, type)
 
 
 class agg:
@@ -370,3 +506,119 @@ class field:
     ) -> SelectionPredicateComposition:
         p = FieldRangePredicate(field=field, range=value, timeUnit=timeUnit)
         return _wrap_composition(p)
+
+
+class field_into:
+    def __init__(self, arg: Map, /) -> None:
+        self._arg: Map = arg
+
+    def angle(self, *args: Any, **kwds: Any) -> channels.Angle: ...
+    def color(self, *args: Any, **kwds: Any) -> channels.Color: ...
+    def column(self, *args: Any, **kwds: Any) -> Any: ...
+    def description(self, *args: Any, **kwds: Any) -> Any: ...
+    def detail(self, *args: Any, **kwds: Any) -> Any: ...
+    def facet(self, *args: Any, **kwds: Any) -> Any: ...
+    def fill(self, *args: Any, **kwds: Any) -> Any: ...
+    def fill_opacity(self, *args: Any, **kwds: Any) -> Any: ...
+    def href(self, *args: Any, **kwds: Any) -> Any: ...
+    def key(self, *args: Any, **kwds: Any) -> Any: ...
+    def latitude(self, *args: Any, **kwds: Any) -> Any: ...
+    def latitude2(self, *args: Any, **kwds: Any) -> Any: ...
+    def longitude(self, *args: Any, **kwds: Any) -> Any: ...
+    def longitude2(self, *args: Any, **kwds: Any) -> Any: ...
+    def opacity(self, *args: Any, **kwds: Any) -> Any: ...
+    def order(self, *args: Any, **kwds: Any) -> Any: ...
+    def radius(self, *args: Any, **kwds: Any) -> Any: ...
+    def radius2(self, *args: Any, **kwds: Any) -> Any: ...
+    def row(self, *args: Any, **kwds: Any) -> Any: ...
+    def shape(self, *args: Any, **kwds: Any) -> Any: ...
+    def size(self, *args: Any, **kwds: Any) -> Any: ...
+    def stroke(self, *args: Any, **kwds: Any) -> Any: ...
+    def stroke_dash(self, *args: Any, **kwds: Any) -> Any: ...
+    def stroke_opacity(self, *args: Any, **kwds: Any) -> Any: ...
+    def stroke_width(self, *args: Any, **kwds: Any) -> Any: ...
+    def text(self, *args: Any, **kwds: Any) -> Any: ...
+    def theta(self, *args: Any, **kwds: Any) -> Any: ...
+    def theta2(self, *args: Any, **kwds: Any) -> Any: ...
+    def tooltip(self, *args: Any, **kwds: Any) -> Any: ...
+    def url(self, *args: Any, **kwds: Any) -> Any: ...
+    def x(self, *args: Any, **kwds: Any) -> channels.X:
+        return channels.X(*args, **self._arg, **kwds)
+
+    def x2(self, *args: Any, **kwds: Any) -> channels.X2:
+        return channels.X2(*args, **self._arg, **kwds)
+
+    def x_error(self, *args: Any, **kwds: Any) -> channels.XError: ...
+    def x_error2(self, *args: Any, **kwds: Any) -> channels.XError2: ...
+    def x_offset(self, *args: Any, **kwds: Any) -> channels.XOffset: ...
+    def y(self, *args: Any, **kwds: Any) -> channels.Y:
+        return channels.Y(*args, **self._arg, **kwds)
+
+    def y2(self, *args: Any, **kwds: Any) -> channels.Y2:
+        return channels.Y2(*args, **self._arg, **kwds)
+
+    def y_error(self, *args: Any, **kwds: Any) -> channels.YError: ...
+    def y_error2(self, *args: Any, **kwds: Any) -> channels.YError2: ...
+    def y_offset(self, *args: Any, **kwds: Any) -> channels.YOffset: ...
+    @property
+    def value(self) -> _ChannelValueNamespace:
+        return _ChannelValueNamespace(self._arg)
+
+
+class _ChannelValueNamespace:
+    def __init__(self, arg: Map, /) -> None:
+        self._arg: Map = arg
+
+    def angle(self, *args: Any, **kwds: Any) -> Any: ...
+    def color(self, *args: Any, **kwds: Any) -> Any: ...
+    def description(self, *args: Any, **kwds: Any) -> Any: ...
+    def fill(self, *args: Any, **kwds: Any) -> Any: ...
+    def fill_opacity(self, *args: Any, **kwds: Any) -> Any: ...
+    def href(self, *args: Any, **kwds: Any) -> Any: ...
+    def latitude2(self, *args: Any, **kwds: Any) -> Any: ...
+    def longitude2(self, *args: Any, **kwds: Any) -> Any: ...
+    def opacity(self, *args: Any, **kwds: Any) -> Any: ...
+    def order(self, *args: Any, **kwds: Any) -> Any: ...
+    def radius(self, *args: Any, **kwds: Any) -> Any: ...
+    def radius2(self, *args: Any, **kwds: Any) -> Any: ...
+    def shape(self, *args: Any, **kwds: Any) -> Any: ...
+    def size(self, *args: Any, **kwds: Any) -> Any: ...
+    def stroke(self, *args: Any, **kwds: Any) -> Any: ...
+    def stroke_dash(self, *args: Any, **kwds: Any) -> Any: ...
+    def stroke_opacity(self, *args: Any, **kwds: Any) -> Any: ...
+    def stroke_width(self, *args: Any, **kwds: Any) -> Any: ...
+    def text(self, *args: Any, **kwds: Any) -> Any: ...
+    def theta(self, *args: Any, **kwds: Any) -> Any: ...
+    def theta2(self, *args: Any, **kwds: Any) -> Any: ...
+    def tooltip(self, *args: Any, **kwds: Any) -> Any: ...
+    def url(self, *args: Any, **kwds: Any) -> Any: ...
+    def x(self, *args: Any, **kwds: Any) -> channels.XValue:
+        return channels.XValue(*args, **self._arg, **kwds)
+
+    def x2(self, *args: Any, **kwds: Any) -> channels.X2Value:
+        return channels.X2Value(*args, **self._arg, **kwds)
+
+    def x_error(self, *args: Any, **kwds: Any) -> channels.XErrorValue: ...
+    def x_error2(self, *args: Any, **kwds: Any) -> channels.XError2Value: ...
+    def x_offset(self, *args: Any, **kwds: Any) -> channels.XOffsetValue: ...
+    def y(self, *args: Any, **kwds: Any) -> channels.YValue:
+        return channels.YValue(*args, **self._arg, **kwds)
+
+    def y2(self, *args: Any, **kwds: Any) -> channels.Y2Value:
+        return channels.Y2Value(*args, **self._arg, **kwds)
+
+    def y_error(self, *args: Any, **kwds: Any) -> channels.YErrorValue: ...
+    def y_error2(self, *args: Any, **kwds: Any) -> channels.YError2Value: ...
+    def y_offset(self, *args: Any, **kwds: Any) -> channels.YOffsetValue: ...
+
+
+# field_out = agg.q1()
+# wrapped = field_into(field_out).value.x()
+## efg = field2.angle()
+## some_field = field2.min("Cost", "Q")
+# some_field = field_into(field("Cost:Q"))
+# beeeee = some_field.x()
+# cee = some_field.value.x()
+#
+#
+# ffff = field_into(field("Cost:Q")).x2().bandPosition(4.7)
