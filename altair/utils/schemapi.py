5e93933789ab51b8e1f625d768046b0bd15c8034
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2024-08-29 17:48:59 +0100

    build: run `generate-schema-wrapper`

--- altair/utils/schemapi.py
+++ altair/utils/schemapi.py
@@ -143,9 +143,7 @@ def debug_mode(arg: bool) -> Iterator[None]:
 
 
 def validate_jsonschema(
-    spec: _JsonParameter,
-    schema: dict[str, Any],
-    rootschema: dict[str, Any] | None = None,
+    spec: _JsonParameter, schema: Map, rootschema: Map | None = None
 ) -> None:
     """
     Validates ``spec`` against ``schema`` in the context of ``rootschema``.
@@ -193,7 +191,7 @@ def validate_jsonschema(
             raise NotImplementedError(msg)
 
 
-def _get_schema_dialect_uri(schema: dict[str, Any]) -> str:
+def _get_schema_dialect_uri(schema: Map, /) -> str:
     """
     Return value of `$schema`_.
 
@@ -206,7 +204,7 @@ def _get_schema_dialect_uri(schema: dict[str, Any]) -> str:
     return schema.get("$schema", _DEFAULT_DIALECT_URI)
 
 
-def _prepare_references(schema: dict[str, Any], /) -> dict[str, Any]:
+def _prepare_references(schema: Map, /) -> dict[str, Any]:
     """
     Return a deep copy of ``schema`` w/ replaced uri(s).
 
@@ -220,7 +218,7 @@ def _prepare_references(schema: dict[str, Any], /) -> dict[str, Any]:
     return dict(_rec_refs(schema))
 
 
-def _rec_refs(m: dict[str, Any], /) -> Iterator[tuple[str, Any]]:
+def _rec_refs(m: Map, /) -> Iterator[tuple[str, Any]]:
     """
     Recurse through a schema, yielding fresh copies of mutable containers.
 
@@ -266,6 +264,7 @@ if Version(importlib_version("jsonschema")) >= Version("4.18"):
     if TYPE_CHECKING:
         from referencing import Specification
         from referencing._core import Resolver
+        from rpds import HashTrieMap
 
     @lru_cache(maxsize=None)
     def specification_with(dialect_id: str, /) -> Specification[Any]:
@@ -287,9 +286,7 @@ if Version(importlib_version("jsonschema")) >= Version("4.18"):
         """
         return _specification_with(dialect_id)
 
-    def _validator(
-        schema: dict[str, Any], rootschema: dict[str, Any] | None = None
-    ) -> Validator:
+    def _validator(schema: Map, rootschema: Map | None = None, /) -> Validator:
         """
         Constructs a `Validator`_ for future validation.
 
@@ -311,7 +308,7 @@ if Version(importlib_version("jsonschema")) >= Version("4.18"):
         registry = _registry(rootschema or schema, uri)
         return validator(_prepare_references(schema), registry=registry)
 
-    def _registry(rootschema: dict[str, Any], dialect_id: str) -> Registry[Any]:
+    def _registry(rootschema: Map, dialect_id: str) -> Registry[Any]:
         """
         Constructs a `Registry`_, adding the `Resource`_ produced by ``rootschema``.
 
@@ -335,16 +332,12 @@ if Version(importlib_version("jsonschema")) >= Version("4.18"):
             _REGISTRY_CACHE[cache_key] = registry
             return registry
 
-    def _registry_update(
-        root: dict[str, Any], dialect_id: str, resolver: Resolver[Any]
-    ):
+    def _registry_update(root: Map, dialect_id: str, resolver: Resolver[Any]) -> None:
         global _REGISTRY_CACHE
         cache_key = _registry_comp_key(root, dialect_id)
         _REGISTRY_CACHE[cache_key] = resolver._registry
 
-    def _resolve_references(
-        schema: dict[str, Any], rootschema: dict[str, Any]
-    ) -> dict[str, Any]:
+    def _resolve_references(schema: Map, rootschema: Map) -> Map:
         """
         Resolve schema references until there is no ``"$ref"`` anymore in the top-level ``dict``.
 
@@ -389,13 +382,27 @@ if Version(importlib_version("jsonschema")) >= Version("4.18"):
             k1 = json.dumps(root, separators=(",", ":"), sort_keys=True)
         return k1, dialect_id
 
+    def resolve_references_rpds(schema: Map, rootschema: Map) -> HashTrieMap[str, Any]:
+        """
+        **Experimental** `rust`-speed returned type.
+
+        Directly wraps `_resolve_references`.
+
+        Idea
+        ----
+        - Store the result of this when called from ``_FromDict.from_dict()`` once per unique call
+        - Reuse the resolved schema, since we don't mutate it after resolving
+        - Should reduce the cost of ``_FromDict.from_dict()``, when a schema has been seen before
+        """
+        import rpds as rpds
+
+        return rpds.HashTrieMap(_resolve_references(schema, rootschema))
+
     _REGISTRY_CACHE: dict[tuple[str, str], Registry[Any]] = {}
 
 else:
 
-    def _validator(
-        schema: dict[str, Any], rootschema: dict[str, Any] | None = None
-    ) -> Validator:
+    def _validator(schema: Map, rootschema: Map | None = None, /) -> Validator:
         """
         Constructs a `Validator`_ for future validation.
 
@@ -418,9 +425,7 @@ else:
         )
         return validator(schema, resolver=resolver)
 
-    def _resolve_references(
-        schema: dict[str, Any], rootschema: dict[str, Any]
-    ) -> dict[str, Any]:
+    def _resolve_references(schema: Map, rootschema: Map) -> Map:
         """
         Resolve schema references until there is no ``"$ref"`` anymore in the top-level ``dict``.
 
@@ -1446,7 +1451,8 @@ class SchemaBase:
             )
             raise TypeError(msg)
         else:
-            return _resolve_references(schema or cls._schema, rootschema=rootschema)
+            resolved = _resolve_references(schema or cls._schema, rootschema)
+            return cast("dict[str, Any]", resolved)
 
     @classmethod
     def validate_property(
