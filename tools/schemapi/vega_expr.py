6bb4c27b9160b9de73936722584b8b70cd7fe34a
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2024-09-19 18:36:45 +0100

    feat: Adds `VegaExprNode`
    Similar to `inspect.Signature`

--- tools/schemapi/vega_expr.py
+++ tools/schemapi/vega_expr.py
@@ -117,6 +117,177 @@ class RSTParse(_RSTParse):
 
 
 parser: RSTParse = RSTParse(RSTRenderer())
+
+@dataclasses.dataclass
+class VegaExprNode:
+    """
+    ``SchemaInfo``-like, but operates on `expressions.md`_.
+
+    .. _expressions.md:
+        https://raw.githubusercontent.com/vega/vega/main/docs/docs/expressions.md
+    """
+
+    name: str
+    _children: Sequence[Token] = dataclasses.field(repr=False)
+    doc: str = ""
+    parameters: list[VegaExprParam] = dataclasses.field(default_factory=list)
+
+    def with_parameters(self) -> Self:
+        raw_texts = self._split_signature_tokens()
+        name = next(raw_texts)
+        # NOTE: Overwriting the <a name> with the rendered text
+        if self.name != name:
+            self.name = name
+        self.parameters = list(VegaExprParam.iter_params(raw_texts))
+        return self
+
+    def with_doc(self) -> Self:
+        self.doc = parser.render_tokens(self._doc_tokens())
+        return self
+
+    @functools.cached_property
+    def parameter_names(self) -> frozenset[str]:
+        if self.parameters:
+            return frozenset(param.name for param in self.parameters)
+        else:
+            msg = (
+                f"Cannot provide `parameter_names` until they have been initialized via:\n"
+                f"{type(self).__name__}.with_parameters()"
+            )
+            raise TypeError(msg)
+
+    @property
+    def name_safe(self) -> str:
+        """Use for the method definition, but not when calling internally."""
+        return f"{self.name}_" if self.is_keyword() else self.name
+
+    def _split_signature_tokens(self) -> Iterator[str]:
+        """Very rough splitting/cleaning of relevant token raw text."""
+        it = iter(self)
+        current = next(it)
+        # NOTE: softbreak(s) denote the line the sig appears on
+        while current[TYPE] != SOFTBREAK:
+            current = next(it)
+        current = next(it)
+        while current[TYPE] != SOFTBREAK:
+            # NOTE: This drops html markup tags
+            if current[TYPE] == TEXT:
+                clean = strip_include_tag(current[RAW]).strip(", -")
+                if clean not in {", ", ""}:
+                    yield from VegaExprNode.deep_split_punctuation(clean)
+            current = next(it, None)
+            if current is None:
+                break
+
+    def _doc_tokens(self) -> Sequence[Token]:
+        """
+        Return the slice of `self.children` that contains docstring content.
+
+        Works for 100% of cases.
+        """
+        for idx, item in enumerate(self):
+            if item[TYPE] == SOFTBREAK and self[idx + 1][TYPE] == TEXT:
+                return self[idx + 1 :]
+            else:
+                continue
+        msg = f"Expected to find a text node marking the start of docstring content.\nFailed for:\n\n{self!r}"
+        raise NotImplementedError(msg)
+
+    @staticmethod
+    def deep_split_punctuation(s: str, /) -> Iterator[str]:
+        """Deep splitting of ending punctuation."""
+        if s.isalnum():
+            yield s
+        else:
+            end: list[str] = []
+            if s.endswith((")", "]")):
+                end.append(s[-1])
+                s = s[:-1]
+            elif s.endswith("..."):
+                end.append(s[-3:])
+                s = s[:-3]
+            elif s.endswith(" |"):
+                end.append(s[-2:])
+                s = s[:-2]
+            if len(s) == 1:
+                yield s
+            elif len(s) > 1:
+                yield from VegaExprNode.deep_split_punctuation(s)
+            yield from end
+
+    def is_callable(self) -> bool:
+        """
+        Rough filter for excluding `constants`_.
+
+        - Most of the parsing is to handle varying signatures.
+        - Constants can just be referenced by name, so can skip those
+
+        .. _constants:
+            https://vega.github.io/vega/docs/expressions/#constants
+        """
+        name = self.name
+        if name.startswith("string_"):
+            # HACK: There are string/array functions that overlap
+            # - the `.md` handles this by prefixing the `<a name=...` for the string version
+            # - however this is another kind of overload
+            # - but with more documentation, than the inline overload for color functions
+            return True
+        elif self.is_bound_variable_name():
+            return False
+        it = iter(self)
+        name = name.lower()
+        current = next(it).get(RAW, "").lower()
+        while current != name:
+            el = next(it, None)
+            if el is None:
+                print(f"Failed match: {name!r}")
+                return True
+            current = el.get(RAW, "").lower()
+        next(it)
+        return next(it).get(RAW, "") == "("
+
+    def is_bound_variable_name(self) -> bool:
+        """
+        ``Vega`` `bound variables`_.
+
+        .. _bound variables:
+            https://vega.github.io/vega/docs/expressions/#bound-variables
+        """
+        RESERVED_NAMES: set[str] = {"datum", "event", "signal"}
+        return self.name in RESERVED_NAMES
+
+    def is_overloaded(self) -> bool:
+        """
+        Covers the `color functions`_.
+
+        These look like:
+
+            lab(l, a, b[, opacity]) | lab(specifier)
+
+        .. _color functions:
+            https://vega.github.io/vega/docs/expressions/#color-functions
+        """
+        for idx, item in enumerate(self):
+            if item[TYPE] == TEXT and item.get(RAW, "") == "]) |":
+                return self[idx + 1][TYPE] == SOFTBREAK
+            else:
+                continue
+        return False
+
+    def is_keyword(self) -> bool:
+        return keyword.iskeyword(self.name)
+
+    def __iter__(self) -> Iterator[Token]:
+        yield from self._children
+
+    @overload
+    def __getitem__(self, index: int) -> Token: ...
+    @overload
+    def __getitem__(self, index: slice) -> Sequence[Token]: ...
+    def __getitem__(self, index: int | slice) -> Token | Sequence[Token]:
+        return self._children.__getitem__(index)
+
+
 @dataclasses.dataclass
 class VegaExprParam:
     name: str
