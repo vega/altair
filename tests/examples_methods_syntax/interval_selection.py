105d5e79f0312410c6c2269ec22bb7a24acb0dbe
ds <63077097+dsmedia@users.noreply.github.com>
2024-10-31 07:38:49 -0400

    docs: adds initial selection range to interval_selection example

--- tests/examples_methods_syntax/interval_selection.py
+++ tests/examples_methods_syntax/interval_selection.py
@@ -3,15 +3,20 @@ Interval Selection
 ==================
 
 This is an example of creating a stacked chart for which the domain of the
-top chart can be selected by interacting with the bottom chart.
+top chart can be selected by interacting with the bottom chart. The initial
+selection range is set using Python's native datetime objects.
 """
 # category: area charts
 import altair as alt
 from vega_datasets import data
+import datetime as dt
 
 source = data.sp500.url
 
-brush = alt.selection_interval(encodings=['x'])
+date_range = (dt.date(2007, 6, 30), dt.date(2009, 6, 30))
+
+brush = alt.selection_interval(encodings=['x'],
+                               value={'x': date_range})
 
 base = alt.Chart(source, width=600, height=200).mark_area().encode(
     x = 'date:T',
