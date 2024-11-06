d8f37918b130d7f89defcb6f1104268db1997420
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2024-11-06 20:24:38 +0000

    refactor: Remove dead code

--- tools/datasets/__init__.py
+++ tools/datasets/__init__.py
@@ -8,11 +8,8 @@ Adapted from `altair-viz/vega_datasets`_.
 from __future__ import annotations
 
 import json
-import tempfile
-from functools import cached_property, partial
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Callable, ClassVar, Literal
-from urllib.request import urlopen
+from typing import TYPE_CHECKING, Any, Literal
 
 import polars as pl
 
@@ -180,167 +177,9 @@ def is_ext_supported(suffix: str) -> TypeIs[Extension]:
     return suffix in {".csv", ".json", ".tsv", ".arrow"}
 
 
-def _py_to_js(s: str, /):
-    return s.replace("_", "-")
-
-
-def _js_to_py(s: str, /):
-    return s.replace("-", "_")
-
-
-class Dataset:
-    read_fn: ClassVar[dict[Extension, Callable[..., pl.DataFrame]]] = {
-        ".csv": pl.read_csv,
-        ".json": pl.read_json,
-        ".tsv": partial(pl.read_csv, separator="\t"),
-        ".arrow": partial(pl.read_ipc, use_pyarrow=True),
-    }
-
-    def __init__(self, name: str, /, base_url: str) -> None:
-        self.name: str = name
-        file_name = DATASETS_JSON[_py_to_js(name)]["filename"]
-        suffix = Path(file_name).suffix
-        if is_ext_supported(suffix):
-            self.extension: Extension = suffix
-        else:
-            raise NotImplementedError(suffix, file_name)
-
-        self.url: str = f"{base_url}{file_name}"
-
-    def __call__(self, **kwds: Any) -> pl.DataFrame:
-        fn = self.read_fn[self.extension]
-        with tempfile.NamedTemporaryFile() as tmp, urlopen(self.url) as f:
-            tmp.write(f.read())
-            content = fn(tmp, **kwds)
-        return content
-
-    def __repr__(self) -> str:
-        return (
-            f"{type(self).__name__}(\n  "
-            f"name={self.name!r},\n  "
-            f"url={self.url!r}\n"
-            ")"
-        )
-
-
-DATASETS_JSON = {
-    # "7zip": {"filename": "7zip.png", "format": "png"},
-    "airports": {"filename": "airports.csv", "format": "csv"},
-    "annual-precip": {"filename": "annual-precip.json", "format": "json"},
-    "anscombe": {"filename": "anscombe.json", "format": "json"},
-    "barley": {"filename": "barley.json", "format": "json"},
-    "birdstrikes": {"filename": "birdstrikes.json", "format": "json"},
-    "budget": {"filename": "budget.json", "format": "json"},
-    "budgets": {"filename": "budgets.json", "format": "json"},
-    "burtin": {"filename": "burtin.json", "format": "json"},
-    "cars": {"filename": "cars.json", "format": "json"},
-    "climate": {"filename": "climate.json", "format": "json"},
-    "co2-concentration": {"filename": "co2-concentration.csv", "format": "csv"},
-    "countries": {"filename": "countries.json", "format": "json"},
-    "crimea": {"filename": "crimea.json", "format": "json"},
-    "disasters": {"filename": "disasters.csv", "format": "csv"},
-    "driving": {"filename": "driving.json", "format": "json"},
-    "earthquakes": {"filename": "earthquakes.json", "format": "json"},
-    # "ffox": {"filename": "ffox.png", "format": "png"},
-    "flare": {"filename": "flare.json", "format": "json"},
-    "flare-dependencies": {"filename": "flare-dependencies.json", "format": "json"},
-    "flights-10k": {"filename": "flights-10k.json", "format": "json"},
-    "flights-200k": {"filename": "flights-200k.json", "format": "json"},
-    "flights-20k": {"filename": "flights-20k.json", "format": "json"},
-    "flights-2k": {"filename": "flights-2k.json", "format": "json"},
-    "flights-3m": {"filename": "flights-3m.csv", "format": "csv"},
-    "flights-5k": {"filename": "flights-5k.json", "format": "json"},
-    "flights-airport": {"filename": "flights-airport.csv", "format": "csv"},
-    "gapminder": {"filename": "gapminder.json", "format": "json"},
-    "gapminder-health-income": {
-        "filename": "gapminder-health-income.csv",
-        "format": "csv",
-    },
-    # "gimp": {"filename": "gimp.png", "format": "png"},
-    "github": {"filename": "github.csv", "format": "csv"},
-    "graticule": {"filename": "graticule.json", "format": "json"},
-    "income": {"filename": "income.json", "format": "json"},
-    "iowa-electricity": {"filename": "iowa-electricity.csv", "format": "csv"},
-    "iris": {"filename": "iris.json", "format": "json"},
-    "jobs": {"filename": "jobs.json", "format": "json"},
-    "la-riots": {"filename": "la-riots.csv", "format": "csv"},
-    "londonBoroughs": {"filename": "londonBoroughs.json", "format": "json"},
-    "londonCentroids": {"filename": "londonCentroids.json", "format": "json"},
-    "londonTubeLines": {"filename": "londonTubeLines.json", "format": "json"},
-    "lookup_groups": {"filename": "lookup_groups.csv", "format": "csv"},
-    "lookup_people": {"filename": "lookup_people.csv", "format": "csv"},
-    "miserables": {"filename": "miserables.json", "format": "json"},
-    "monarchs": {"filename": "monarchs.json", "format": "json"},
-    "movies": {"filename": "movies.json", "format": "json"},
-    "normal-2d": {"filename": "normal-2d.json", "format": "json"},
-    "obesity": {"filename": "obesity.json", "format": "json"},
-    "ohlc": {"filename": "ohlc.json", "format": "json"},
-    "points": {"filename": "points.json", "format": "json"},
-    "population": {"filename": "population.json", "format": "json"},
-    "population_engineers_hurricanes": {
-        "filename": "population_engineers_hurricanes.csv",
-        "format": "csv",
-    },
-    "seattle-temps": {"filename": "seattle-temps.csv", "format": "csv"},
-    "seattle-weather": {"filename": "seattle-weather.csv", "format": "csv"},
-    "sf-temps": {"filename": "sf-temps.csv", "format": "csv"},
-    "sp500": {"filename": "sp500.csv", "format": "csv"},
-    "stocks": {"filename": "stocks.csv", "format": "csv"},
-    "udistrict": {"filename": "udistrict.json", "format": "json"},
-    "unemployment": {"filename": "unemployment.tsv", "format": "tsv"},
-    "unemployment-across-industries": {
-        "filename": "unemployment-across-industries.json",
-        "format": "json",
-    },
-    "uniform-2d": {"filename": "uniform-2d.json", "format": "json"},
-    "us-10m": {"filename": "us-10m.json", "format": "json"},
-    "us-employment": {"filename": "us-employment.csv", "format": "csv"},
-    "us-state-capitals": {"filename": "us-state-capitals.json", "format": "json"},
-    "volcano": {"filename": "volcano.json", "format": "json"},
-    "weather": {"filename": "weather.json", "format": "json"},
-    "weball26": {"filename": "weball26.json", "format": "json"},
-    "wheat": {"filename": "wheat.json", "format": "json"},
-    "windvectors": {"filename": "windvectors.csv", "format": "csv"},
-    "world-110m": {"filename": "world-110m.json", "format": "json"},
-    "zipcodes": {"filename": "zipcodes.csv", "format": "csv"},
-}
-"""Inlined `datasets.json`_.
-
-- Excluding images
-
-.. _datasets.json:
-    https://github.com/altair-viz/vega_datasets/blob/136e850447b49031f04baa137ce5c37a6678bbb1/vega_datasets/datasets.json
-"""
-
-
 class DataLoader:
-    source_tag: ClassVar[str] = "v2.9.0"
-    _base_url_fmt: str = "https://cdn.jsdelivr.net/npm/vega-datasets@{0}/data/"
-
-    @property
-    def base_url(self) -> str:
-        return self._base_url_fmt.format(self.source_tag)
-
-    @cached_property
-    def _dataset_names(self) -> list[str]:
-        return sorted(DATASETS_JSON)
-
-    @cached_property
-    def _py_js_names(self) -> dict[str, str]:
-        return {_js_to_py(name): name for name in self._dataset_names}
-
-    def list_datasets(self) -> list[str]:
-        return list(self._py_js_names)
-
-    def __getattr__(self, name: str) -> Dataset:
-        if name in self._py_js_names:
-            return Dataset(self._py_js_names[name], self.base_url)
-        else:
-            msg = f"No dataset named {name!r}"
-            raise AttributeError(msg)
-
-    def __dir__(self) -> list[str]:
-        return self.list_datasets()
+    def __init__(self, application: Application, /) -> None:
+        self._app: Application = application
 
     def url(
         self,
@@ -367,7 +206,7 @@ class DataLoader:
             else:
                 constraints["suffix"] = ext
         q = QueryTree(name_js=name, **constraints)  # type: ignore[typeddict-item]
-        return app.github.query.url_from(**q)
+        return self._app.github.query.url_from(**q)
 
     def __call__(
         self,
@@ -384,7 +223,7 @@ class DataLoader:
         - Will only be using the actual (js_name)
         - Some have hyphens, others underscores
         """
-        return app.npm.dataset(self.url(name, ext, tag=tag), **kwds)
+        return self._app.npm.dataset(self.url(name, ext, tag=tag), **kwds)
 
 
-data = DataLoader()
+data = DataLoader(app)
