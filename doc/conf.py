9209e7e47b34b755e3a199d323ac9a2d9b881fef
Jon Mease <jonmmease@gmail.com>
2024-11-23 15:38:38 -0500

    bump version to 5.5.0

--- doc/conf.py
+++ doc/conf.py
@@ -79,7 +79,7 @@ author = "Vega-Altair Developers"
 # built documents.
 #
 # The short X.Y version.
-version = "5.5.0dev"
+version = "5.5.0"
 # The full version, including alpha/beta/rc tags.
 release = f"{version}"
 
