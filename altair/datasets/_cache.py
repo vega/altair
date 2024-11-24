71423eadfe63a767c2b591f743b3a36272d59c7d
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2024-11-24 15:11:41 +0000

    refactor: Tidy up `DatasetCache`

--- altair/datasets/_cache.py
+++ altair/datasets/_cache.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING, ClassVar, Generic, TypeVar, get_args
 
 import narwhals.stable.v1 as nw
-from narwhals.stable.v1 import dependencies as nw_dep
+from narwhals.stable.v1.dependencies import get_pyarrow
 from narwhals.stable.v1.typing import IntoDataFrameT, IntoFrameT
 
 from altair.datasets._typing import VERSION_LATEST
@@ -102,22 +102,38 @@ class DatasetCache(Generic[IntoDataFrameT, IntoFrameT]):
     def __init__(self, reader: _Reader[IntoDataFrameT, IntoFrameT], /) -> None:
         self._rd: _Reader[IntoDataFrameT, IntoFrameT] = reader
 
+    def clear(self) -> None:
+        """Delete all previously cached datasets."""
+        self._ensure_active()
+        if self.is_empty():
+            return None
+        ser = (
+            self._rd._scan_metadata()
+            .select("sha", "suffix")
+            .unique("sha")
+            .select(nw.concat_str("sha", "suffix").alias("sha_suffix"))
+            .collect()
+            .get_column("sha_suffix")
+        )
+        names = set[str](
+            ser.to_list() if nw.get_native_namespace(ser) is get_pyarrow() else ser
+        )
+        for fp in self:
+            if fp.name in names:
+                fp.unlink()
+
     def download_all(self) -> None:
         """
         Download any missing datasets for latest version.
 
-        ``v2.11.0`` stats
-        -----------------
-        - **66** items
-        - **27.8** MB
-        - Only 1 file > 2 MB
+        Requires **30-50MB** of disk-space.
         """
         stems = tuple(fp.stem for fp in self)
         latest = nw.col("tag") == nw.lit(VERSION_LATEST)
         predicates = (~(nw.col("sha").is_in(stems)), latest) if stems else (latest,)
         frame = (
             self._rd._scan_metadata(
-                *predicates, ext_supported=True, name_collision=False
+                predicates, ext_supported=True, name_collision=False
             )
             .select("sha", "suffix", "url_npm")
             .unique("sha")
@@ -134,65 +150,6 @@ class DatasetCache(Generic[IntoDataFrameT, IntoFrameT]):
                 fp.write_bytes(f.read())
         print("Finished downloads")
         return None
-
-    def clear(self) -> None:
-        # unlink all matching sha
-        # stricter than `__iter__`
-        # - to avoid deleting unrelated files in dir
-        self.ensure_active()
-        if self.is_empty():
-            return None
-        ser = (
-            self._rd._scan_metadata()
-            .select("sha", "suffix")
-            .unique("sha")
-            .select(nw.concat_str("sha", "suffix").alias("sha_suffix"))
-            .collect()
-            .get_column("sha_suffix")
-        )
-        names = set[str](
-            ser.to_list()
-            if nw.get_native_namespace(ser) is nw_dep.get_pyarrow()
-            else ser
-        )
-        for fp in self:
-            if fp.name in names:
-                fp.unlink()
-
-    def __iter__(self) -> Iterator[Path]:
-        yield from self.path.iterdir()
-
-    def __repr__(self):
-        name = type(self).__name__
-        if self.is_not_active():
-            return f"{name}<UNSET>"
-        else:
-            return f"{name}<{self.path.as_posix()!r}>"
-
-    def is_active(self) -> bool:
-        return not self.is_not_active()
-
-    def is_not_active(self) -> bool:
-        return os.environ.get(self._ENV_VAR) is None
-
-    def is_empty(self) -> bool:
-        """Cache is active, but no files in the directory."""
-        return next(iter(self), None) is None
-
-    def ensure_active(self) -> None:
-        # Fail fast when the cache op is later
-        # Otherwise, just get the error from `self.path`
-        if self.is_not_active():
-            msg = (
-                f"Cache is unset.\n"
-                f"To enable dataset caching, set the environment variable:\n"
-                f"    {self._ENV_VAR!r}\n\n"
-                f"You can set this for the current session via:\n"
-                f"    from pathlib import Path\n"
-                f"    from altair.datasets import load\n\n"
-                f"    load.cache.path = Path.home() / '.altair_cache'"
-            )
-            raise ValueError(msg)
 
     @property
     def path(self) -> Path:
@@ -216,7 +173,7 @@ class DatasetCache(Generic[IntoDataFrameT, IntoFrameT]):
 
             >>> load.cache.path = None
         """
-        self.ensure_active()
+        self._ensure_active()
         fp = Path(os.environ[self._ENV_VAR])
         fp.mkdir(exist_ok=True)
         return fp
@@ -227,6 +184,39 @@ class DatasetCache(Generic[IntoDataFrameT, IntoFrameT]):
             os.environ[self._ENV_VAR] = str(Path(source).resolve())
         else:
             os.environ.pop(self._ENV_VAR, None)
+
+    def __iter__(self) -> Iterator[Path]:
+        yield from self.path.iterdir()
+
+    def __repr__(self) -> str:
+        name = type(self).__name__
+        if self.is_not_active():
+            return f"{name}<UNSET>"
+        else:
+            return f"{name}<{self.path.as_posix()!r}>"
+
+    def is_active(self) -> bool:
+        return not self.is_not_active()
+
+    def is_not_active(self) -> bool:
+        return os.environ.get(self._ENV_VAR) is None
+
+    def is_empty(self) -> bool:
+        """Cache is active, but no files are stored in ``self.path``."""
+        return next(iter(self), None) is None
+
+    def _ensure_active(self) -> None:
+        if self.is_not_active():
+            msg = (
+                f"Cache is unset.\n"
+                f"To enable dataset caching, set the environment variable:\n"
+                f"    {self._ENV_VAR!r}\n\n"
+                f"You can set this for the current session via:\n"
+                f"    from pathlib import Path\n"
+                f"    from altair.datasets import load\n\n"
+                f"    load.cache.path = Path.home() / '.altair_cache'"
+            )
+            raise ValueError(msg)
 
 
 url_cache: UrlCache[Dataset | LiteralString, str] = UrlCache(_URL)
