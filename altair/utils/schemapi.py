fe5c8409883f8ed3c89e6e1a7e5a1bf5e1a34219
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-01-02 12:35:35 +0000

    feat: Show python types in `ValidationError` messages
    Closes #2914

--- altair/utils/schemapi.py
+++ altair/utils/schemapi.py
@@ -7,6 +7,7 @@ import copy
 import datetime as dt
 import inspect
 import json
+import operator
 import sys
 import textwrap
 from collections import defaultdict
@@ -48,6 +49,7 @@ if TYPE_CHECKING:
     from types import ModuleType
     from typing import ClassVar
 
+    from jsonschema.exceptions import ValidationError
     from referencing import Registry
 
     from altair.typing import ChartType
@@ -589,7 +591,23 @@ def _resolve_references(
     return schema
 
 
+def _validator_values(errors: Iterable[ValidationError], /) -> Iterator[str]:
+    """Unwrap each error's ``.validator_value``, convince ``mypy`` it stores a string."""
+    for err in errors:
+        yield cast("str", err.validator_value)
+
+
 class SchemaValidationError(jsonschema.ValidationError):
+    _JS_TO_PY: ClassVar[Mapping[str, str]] = {
+        "boolean": "bool",
+        "integer": "int",
+        "number": "float",
+        "string": "str",
+        "null": "None",
+        "object": "Mapping[str, Any]",
+        "array": "Sequence",
+    }
+
     def __init__(self, obj: SchemaBase, err: jsonschema.ValidationError) -> None:
         """
         A wrapper for ``jsonschema.ValidationError`` with friendlier traceback.
@@ -762,26 +780,39 @@ See the help for `{altair_cls.__name__}` to read the full description of these p
                     param_names_table += "\n"
         return param_names_table
 
+    def _format_type_reprs(self, errors: Iterable[ValidationError], /) -> str:
+        """
+        Translate jsonschema types to how they appear in annotations.
+
+        Adapts parts of:
+        - `tools.schemapi.utils.sort_type_reprs`_
+        - `tools.schemapi.utils.SchemaInfo.to_type_repr`_
+
+        .. _tools.schemapi.utils.sort_type_reprs:
+            https://github.com/vega/altair/blob/48e976ef9388ce08a2e871a0f67ed012b914597a/tools/schemapi/utils.py#L1106-L1146
+        .. _tools.schemapi.utils.SchemaInfo.to_type_repr:
+            https://github.com/vega/altair/blob/48e976ef9388ce08a2e871a0f67ed012b914597a/tools/schemapi/utils.py#L449-L543
+        """
+        to_py_types = (
+            self._JS_TO_PY.get(val, val) for val in _validator_values(errors)
+        )
+        it = sorted(to_py_types, key=str.lower)
+        it = sorted(it, key=len)
+        it = sorted(it, key=partial(operator.eq, "None"))
+        return f"of type `{' | '.join(it)}`"
+
     def _get_default_error_message(
         self,
         errors: ValidationErrorList,
     ) -> str:
         bullet_points: list[str] = []
         errors_by_validator = _group_errors_by_validator(errors)
-        if "enum" in errors_by_validator:
-            for error in errors_by_validator["enum"]:
-                bullet_points.append(f"one of {error.validator_value}")
-
-        if "type" in errors_by_validator:
-            types = [f"'{err.validator_value}'" for err in errors_by_validator["type"]]
-            point = "of type "
-            if len(types) == 1:
-                point += types[0]
-            elif len(types) == 2:
-                point += f"{types[0]} or {types[1]}"
-            else:
-                point += ", ".join(types[:-1]) + f", or {types[-1]}"
-            bullet_points.append(point)
+        if errs_enum := errors_by_validator.get("enum", None):
+            bullet_points.extend(
+                f"one of {val}" for val in _validator_values(errs_enum)
+            )
+        if errs_type := errors_by_validator.get("type", None):
+            bullet_points.append(self._format_type_reprs(errs_type))
 
         # It should not matter which error is specifically used as they are all
         # about the same offending instance (i.e. invalid value), so we can just
