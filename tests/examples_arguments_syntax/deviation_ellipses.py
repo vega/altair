1983ede5160d5f71d374066f3f59645261545e18
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2025-01-04 17:35:52 +0000

    docs: Initial rewrite of (#514)
    Happy with the end result, but not comfortable merging so much complexity I don't understand yet
    
    #3715

--- tests/examples_arguments_syntax/deviation_ellipses.py
+++ tests/examples_arguments_syntax/deviation_ellipses.py
@@ -1,63 +1,89 @@
+from __future__ import annotations
+
 """
 Deviation ellipse
 -----------------------
 This example shows bivariate deviation ellipses of petal length and width of three iris species.
 """
 
+from typing import Literal
+
 import numpy as np
 import pandas as pd
 from scipy.stats import f as ssf
+
 import altair as alt
 from vega_datasets import data
 
-iris = data.iris()
 
-def ellipse(X, level=0.95, method='deviation', npoints=100):
+def np_ellipse(
+    arr: np.ndarray[tuple[int, int], np.dtype[np.float64]],
+    conf_level: float = 0.95,
+    method: Literal["deviation", "error"] = "deviation",
+    segments: int = 100,
+):
     """
-    X: data, 2D numpy array with 2 columns
-    level: confidence level
-    method: either 'deviation' (swarning data) or 'error (swarning the mean)'
-    npoints: number of points describing the ellipse
-    """
-    cov_mat = np.cov(X.T)
-    dfd = X.shape[0]-1
+    Arguments:
+        arr:  2D numpy array with 2 columns
+        level: confidence level
+        method: either 'deviation' (swarning data) or 'error (swarning the mean)'
+        segments: number of points describing the ellipse.
+    """  # noqa: D205
+    n_elements = len(arr)
+    # TODO: 
+    # - dfn?
+    # - dfd?
+    # - ssf.ppf?
+    #   - Percent point function (inverse of `cdf`) at q of the given RV.
     dfn = 2
-    center = np.apply_along_axis(np.mean, arr=X, axis=0) # np.mean(X, axis=0)
-    if method == 'deviation':
-        radius = np.sqrt(2 * ssf.ppf(q=level, dfn=dfn, dfd=dfd))
-    elif method == 'error':
-        radius = np.sqrt(2 * ssf.ppf(q=level, dfn=dfn, dfd=dfd)) / np.sqrt(X.shape[0])
+    dfd = n_elements - 1
+    deviation = np.sqrt(2 * ssf.ppf(conf_level, dfn=dfn, dfd=dfd))
+    if method == "deviation":
+        radius = deviation
+    elif method == "error":
+        radius = deviation / np.sqrt(n_elements)
     else:
-        raise ValueError("Method should be either 'deviation' or 'error'.")
-    angles = (np.arange(0,npoints+1)) * 2 * np.pi/npoints
+        msg = "Method should be either 'deviation' or 'error'."
+        raise ValueError(msg)
+    angles = (np.arange(0, segments + 1)) * 2 * np.pi / segments
     circle = np.vstack((np.cos(angles), np.sin(angles))).T
+    center = np.mean(arr, axis=0)
+    cov_mat = np.cov(arr.T)
+    # TODO: Figure out why so many transpositions
     ellipse = center + (radius * np.dot(circle, np.linalg.cholesky(cov_mat).T).T).T
     return ellipse
 
-columns = ['petalLength', 'petalWidth']
-petal_ellipse = []
-for species in iris.species.unique():
-    ell_df = pd.DataFrame(ellipse(X=iris.loc[iris.species == species, columns].as_matrix()),
-                                      columns = columns)
-    ell_df['species'] = species
-    petal_ellipse.append(ell_df)
 
-petal_ellipse = pd.concat(petal_ellipse, axis=0).reset_index()
+def pd_ellipse(
+    df: pd.DataFrame, col_x: str, col_y: str, col_group: str
+) -> pd.DataFrame:
+    cols = col_x, col_y
+    groups = []
+    # TODO: Rewrite in a more readable way
+    categories = df[col_group].unique()
+    for category in categories:
+        sliced = df.loc[df[col_group] == category, cols]
+        ell_df = pd.DataFrame(np_ellipse(sliced.to_numpy()), columns=cols)
+        ell_df[col_group] = category
+        groups.append(ell_df)
+    return pd.concat(groups).reset_index()
 
-chart_deviation = alt.Chart(petal_ellipse).mark_line().\
-    encode(
-        x=alt.X('petalLength', scale=alt.Scale(zero=False)),
-        y=alt.Y('petalWidth', scale=alt.Scale(zero=False)),
-        color='species',
-        order='index')
-# works only if chart_deviation is printed here
-# chart_deviation
 
-chart_points = alt.Chart(iris).mark_point().\
-    encode(
-        x=alt.X('petalLength', scale=alt.Scale(zero=False)),
-        y=alt.Y('petalWidth', scale=alt.Scale(zero=False)),
-        color='species')
+col_x = "petalLength"
+col_y = "petalWidth"
+col_group = "species"
+x = alt.X(col_x, scale=alt.Scale(zero=False))
+y = alt.Y(col_y, scale=alt.Scale(zero=False))
+color = alt.Color(col_group)
 
-chart = chart_points + chart_deviation
+source = data.iris()
+ellipse = pd_ellipse(source, col_x=col_x, col_y=col_y, col_group=col_group)
+points = alt.Chart(source).mark_circle().encode(x, y, color)
+lines = (
+    alt.Chart(ellipse)
+    .mark_line(filled=True, fillOpacity=0.2)
+    .encode(x, y, color, order="index")
+)
+
+chart = lines + points
 chart
