246b4eed1d4057af8b75df3a5f024c4330158b7f
Mattijn van Hoek <mattijn@gmail.com>
2025-06-03 22:27:32 +0200

    picnic?

--- tests/utils/test_mimebundle.py
+++ tests/utils/test_mimebundle.py
@@ -175,7 +175,7 @@ def test_vegalite_to_vega_mimebundle(vegalite_spec, vega_spec):
         engine="vl-convert",
     )
 
-    assert bundle == {"application/vnd.vega.v6json": vega_spec}
+    assert bundle == {"application/vnd.vega.v6+json": vega_spec}
 
 
 def test_spec_to_vegalite_mimebundle(vegalite_spec):
@@ -235,7 +235,7 @@ def test_vegafusion_spec_to_vega_mime_bundle(vegalite_spec):
             format="vega",
         )
         # Returned bundle will be vega
-        vega_spec = bundle["application/vnd.vega.v6json"]
+        vega_spec = bundle["application/vnd.vega.v6+json"]
         check_pre_transformed_vega_spec(vega_spec)
 
 
