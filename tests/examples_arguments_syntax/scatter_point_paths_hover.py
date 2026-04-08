0eb79299d4f16b956a8f1a78716b6e97162bb537
Joel Ostblom <joel.ostblom@gmail.com>
2026-04-08 10:20:04 +0200

    Impreove wording and formatting

--- tests/examples_arguments_syntax/scatter_point_paths_hover.py
+++ tests/examples_arguments_syntax/scatter_point_paths_hover.py
@@ -1,6 +1,6 @@
 """
-Scatter plot with point paths on hover with search box
-======================================================
+Scatter plot with point paths on hover and search box
+=====================================================
 This example combines cross-sectional analysis (comparing countries at a single point in time) 
 with longitudinal analysis (tracking changes in individual countries over time), using
 an interactive visualization technique inspired by [this Vega example](https://vega.github.io/vega/examples/global-development/).
