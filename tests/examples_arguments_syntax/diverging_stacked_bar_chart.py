91bc1d67bf053c9ed14cb451f30a1ec433a82359
Joel Ostblom <joel.ostblom@gmail.com>
2026-06-13 17:41:27 +0200

    docs: Swap pandas for polars to avoid py3.10 issue

--- tests/examples_arguments_syntax/diverging_stacked_bar_chart.py
+++ tests/examples_arguments_syntax/diverging_stacked_bar_chart.py
@@ -5,248 +5,243 @@ This example shows a diverging stacked bar chart for sentiments towards a set of
 """
 # category: bar charts
 import altair as alt
-import pandas as pd
+import polars as pl
+import polars.selectors as cs
 
-
-source = pd.DataFrame(
-    [
-        {
-            "question": "Question 1",
-            "type": "Strongly disagree",
-            "value": 24,
-        },
-        {
-            "question": "Question 1",
-            "type": "Disagree",
-            "value": 294,
-        },
-        {
-            "question": "Question 1",
-            "type": "Neither agree nor disagree",
-            "value": 594,
-        },
-        {
-            "question": "Question 1",
-            "type": "Agree",
-            "value": 1927,
-        },
-        {
-            "question": "Question 1",
-            "type": "Strongly agree",
-            "value": 376,
-        },
-        {
-            "question": "Question 2",
-            "type": "Strongly disagree",
-            "value": 2,
-        },
-        {
-            "question": "Question 2",
-            "type": "Disagree",
-            "value": 2,
-        },
-        {
-            "question": "Question 2",
-            "type": "Neither agree nor disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 2",
-            "type": "Agree",
-            "value": 7,
-        },
-        {
-            "question": "Question 2",
-            "type": "Strongly agree",
-            "value": 11,
-        },
-        {
-            "question": "Question 3",
-            "type": "Strongly disagree",
-            "value": 2,
-        },
-        {
-            "question": "Question 3",
-            "type": "Disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 3",
-            "type": "Neither agree nor disagree",
-            "value": 2,
-        },
-        {
-            "question": "Question 3",
-            "type": "Agree",
-            "value": 4,
-        },
-        {
-            "question": "Question 3",
-            "type": "Strongly agree",
-            "value": 2,
-        },
-        {
-            "question": "Question 4",
-            "type": "Strongly disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 4",
-            "type": "Disagree",
-            "value": 2,
-        },
-        {
-            "question": "Question 4",
-            "type": "Neither agree nor disagree",
-            "value": 1,
-        },
-        {
-            "question": "Question 4",
-            "type": "Agree",
-            "value": 7,
-        },
-        {
-            "question": "Question 4",
-            "type": "Strongly agree",
-            "value": 6,
-        },
-        {
-            "question": "Question 5",
-            "type": "Strongly disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 5",
-            "type": "Disagree",
-            "value": 1,
-        },
-        {
-            "question": "Question 5",
-            "type": "Neither agree nor disagree",
-            "value": 3,
-        },
-        {
-            "question": "Question 5",
-            "type": "Agree",
-            "value": 16,
-        },
-        {
-            "question": "Question 5",
-            "type": "Strongly agree",
-            "value": 4,
-        },
-        {
-            "question": "Question 6",
-            "type": "Strongly disagree",
-            "value": 1,
-        },
-        {
-            "question": "Question 6",
-            "type": "Disagree",
-            "value": 1,
-        },
-        {
-            "question": "Question 6",
-            "type": "Neither agree nor disagree",
-            "value": 2,
-        },
-        {
-            "question": "Question 6",
-            "type": "Agree",
-            "value": 9,
-        },
-        {
-            "question": "Question 6",
-            "type": "Strongly agree",
-            "value": 3,
-        },
-        {
-            "question": "Question 7",
-            "type": "Strongly disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 7",
-            "type": "Disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 7",
-            "type": "Neither agree nor disagree",
-            "value": 1,
-        },
-        {
-            "question": "Question 7",
-            "type": "Agree",
-            "value": 4,
-        },
-        {
-            "question": "Question 7",
-            "type": "Strongly agree",
-            "value": 0,
-        },
-        {
-            "question": "Question 8",
-            "type": "Strongly disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 8",
-            "type": "Disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 8",
-            "type": "Neither agree nor disagree",
-            "value": 0,
-        },
-        {
-            "question": "Question 8",
-            "type": "Agree",
-            "value": 0,
-        },
-        {
-            "question": "Question 8",
-            "type": "Strongly agree",
-            "value": 2,
-        },
-    ]
-)
-
-
-# Add type_code that we can sort by
-source["type_code"] = source["type"].map(
+records = [
     {
-        "Strongly disagree": -2,
-        "Disagree": -1,
-        "Neither agree nor disagree": 0,
-        "Agree": 1,
-        "Strongly agree": 2,
-    }
+        "question": "Question 1",
+        "type": "Strongly disagree",
+        "value": 24,
+    },
+    {
+        "question": "Question 1",
+        "type": "Disagree",
+        "value": 294,
+    },
+    {
+        "question": "Question 1",
+        "type": "Neither agree nor disagree",
+        "value": 594,
+    },
+    {
+        "question": "Question 1",
+        "type": "Agree",
+        "value": 1927,
+    },
+    {
+        "question": "Question 1",
+        "type": "Strongly agree",
+        "value": 376,
+    },
+    {
+        "question": "Question 2",
+        "type": "Strongly disagree",
+        "value": 2,
+    },
+    {
+        "question": "Question 2",
+        "type": "Disagree",
+        "value": 2,
+    },
+    {
+        "question": "Question 2",
+        "type": "Neither agree nor disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 2",
+        "type": "Agree",
+        "value": 7,
+    },
+    {
+        "question": "Question 2",
+        "type": "Strongly agree",
+        "value": 11,
+    },
+    {
+        "question": "Question 3",
+        "type": "Strongly disagree",
+        "value": 2,
+    },
+    {
+        "question": "Question 3",
+        "type": "Disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 3",
+        "type": "Neither agree nor disagree",
+        "value": 2,
+    },
+    {
+        "question": "Question 3",
+        "type": "Agree",
+        "value": 4,
+    },
+    {
+        "question": "Question 3",
+        "type": "Strongly agree",
+        "value": 2,
+    },
+    {
+        "question": "Question 4",
+        "type": "Strongly disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 4",
+        "type": "Disagree",
+        "value": 2,
+    },
+    {
+        "question": "Question 4",
+        "type": "Neither agree nor disagree",
+        "value": 1,
+    },
+    {
+        "question": "Question 4",
+        "type": "Agree",
+        "value": 7,
+    },
+    {
+        "question": "Question 4",
+        "type": "Strongly agree",
+        "value": 6,
+    },
+    {
+        "question": "Question 5",
+        "type": "Strongly disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 5",
+        "type": "Disagree",
+        "value": 1,
+    },
+    {
+        "question": "Question 5",
+        "type": "Neither agree nor disagree",
+        "value": 3,
+    },
+    {
+        "question": "Question 5",
+        "type": "Agree",
+        "value": 16,
+    },
+    {
+        "question": "Question 5",
+        "type": "Strongly agree",
+        "value": 4,
+    },
+    {
+        "question": "Question 6",
+        "type": "Strongly disagree",
+        "value": 1,
+    },
+    {
+        "question": "Question 6",
+        "type": "Disagree",
+        "value": 1,
+    },
+    {
+        "question": "Question 6",
+        "type": "Neither agree nor disagree",
+        "value": 2,
+    },
+    {
+        "question": "Question 6",
+        "type": "Agree",
+        "value": 9,
+    },
+    {
+        "question": "Question 6",
+        "type": "Strongly agree",
+        "value": 3,
+    },
+    {
+        "question": "Question 7",
+        "type": "Strongly disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 7",
+        "type": "Disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 7",
+        "type": "Neither agree nor disagree",
+        "value": 1,
+    },
+    {
+        "question": "Question 7",
+        "type": "Agree",
+        "value": 4,
+    },
+    {
+        "question": "Question 7",
+        "type": "Strongly agree",
+        "value": 0,
+    },
+    {
+        "question": "Question 8",
+        "type": "Strongly disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 8",
+        "type": "Disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 8",
+        "type": "Neither agree nor disagree",
+        "value": 0,
+    },
+    {
+        "question": "Question 8",
+        "type": "Agree",
+        "value": 0,
+    },
+    {
+        "question": "Question 8",
+        "type": "Strongly agree",
+        "value": 2,
+    },
+]
+
+source = pl.DataFrame(records)
+type_codes = {
+    "Strongly disagree": -2,
+    "Disagree": -1,
+    "Neither agree nor disagree": 0,
+    "Agree": 1,
+    "Strongly agree": 2,
+}
+
+value = pl.col("value")
+perc = pl.col("percentage")
+sum_disagree = perc.filter(pl.col.type.is_in(["Strongly disagree", "Disagree"])).sum()
+percentage_end = perc.cum_sum() - (
+    sum_disagree + perc.filter(type="Neither agree nor disagree").item() / 2
 )
 
-
-def compute_percentages(
-    group,
-):
-    # Set type_code as index and sort
-    group = group.set_index("type_code").sort_index()
-
-    # Compute percentage of value with question group
-    perc = (group["value"] / group["value"].sum()) * 100
-    group["percentage"] = perc
-
-    # Compute percentage end, centered on "Neither agree nor disagree" (type_code 0)
-    # Note that we access the perc series via index which is based on 'type_code'.
-    group["percentage_end"] = perc.cumsum() - (perc[-2] + perc[-1] + perc[0] / 2)
-
-    # Compute percentage start by subtracting percent
-    group["percentage_start"] = group["percentage_end"] - perc
-
-    return group
-
-
-source = source.groupby("question").apply(compute_percentages).reset_index()
+source = (
+    pl.DataFrame(records)
+    .with_columns(
+        type_code=pl.col.type.replace_strict(type_codes),
+        percentage=((value / value.sum()) * 100).over("question"),
+    )
+    .group_by("question")
+    .agg(
+        pl.all(),
+        percentage_end=percentage_end,
+        percentage_start=(percentage_end - perc),
+    )
+    .explode(cs.list())
+    .sort("question", "type_code")
+)
 
 
 color_scale = alt.Scale(
