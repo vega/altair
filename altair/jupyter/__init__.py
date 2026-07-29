4e908aa70b5be6c90f24538f83e39c1bcf8b99dd
Joshua Nwachinemere <217677783+dk3yyyy@users.noreply.github.com>
2026-07-29 21:11:29 +0100

    fix: report missing anywidget for JupyterChart (#4089)
    * fix: report missing anywidget for JupyterChart
    
    * test: use no-anywidget CI coverage

--- altair/jupyter/__init__.py
+++ altair/jupyter/__init__.py
@@ -1,21 +1,34 @@
-try:
-    import anywidget  # noqa: F401
-except ImportError:
-    # When anywidget isn't available, create stand-in JupyterChart class
-    # that raises an informative import error on construction. This
+from importlib.util import find_spec
+from typing import TYPE_CHECKING, NoReturn
+
+_ANYWIDGET_IMPORT_ERROR = (
+    "The Altair JupyterChart requires the anywidget \n"
+    "Python package which may be installed using pip with\n"
+    "    pip install anywidget\n"
+    "or using conda with\n"
+    "    conda install -c conda-forge anywidget\n"
+    "Afterwards, you will need to restart your Python kernel."
+)
+
+
+def _raise_anywidget_import_error() -> NoReturn:
+    raise ImportError(_ANYWIDGET_IMPORT_ERROR)
+
+
+if TYPE_CHECKING:
+    from .jupyter_chart import JupyterChart
+elif find_spec("anywidget") is None:
+    # When anywidget isn't available, create a stand-in JupyterChart class
+    # that raises an informative import error when its API is used. This
     # way we can make JupyterChart available in the altair namespace
-    # when anywidget is not installed
+    # when anywidget is not installed.
     class JupyterChart:
-        def __init__(self, *args, **kwargs):
-            msg = (
-                "The Altair JupyterChart requires the anywidget \n"
-                "Python package which may be installed using pip with\n"
-                "    pip install anywidget\n"
-                "or using conda with\n"
-                "    conda install -c conda-forge anywidget\n"
-                "Afterwards, you will need to restart your Python kernel."
-            )
-            raise ImportError(msg)
+        def __init__(self, *args: object, **kwargs: object) -> NoReturn:
+            _raise_anywidget_import_error()
+
+        @classmethod
+        def enable_offline(cls, offline: bool = True) -> NoReturn:
+            _raise_anywidget_import_error()
 
 else:
-    from .jupyter_chart import JupyterChart  # noqa: F401
+    from .jupyter_chart import JupyterChart as JupyterChart
