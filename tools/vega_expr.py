df448e233cd0aaf69d3a82e0c79ccd643b710edc
Alec Robertson <fencing-suits0i@icloud.com>
2026-09-11 10:44:27 -0700

    Fix type error instead of ignoring it

--- tools/vega_expr.py
+++ tools/vega_expr.py
@@ -384,7 +384,7 @@ class ReplaceMany:
 
     def _compile(self) -> Pattern[str]:
         if not self._mapping:
-            name = self._mapping.__qualname__  # type: ignore
+            name = type(self._mapping).__qualname__
             msg = (
                 f"Requires {name!r} to be populated, but got:\n{name}={self._mapping!r}"
             )
